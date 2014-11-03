3e6888208689f7739adcf996561aec1cef039a51
Alex Henderson <alex.henderson@manchester.ac.uk>
2014-11-03 19:12:34 +0000

    Added simple tests for cloneability.

---
+++ tests/test_ChiCloneable.m
@@ -0,0 +1,19 @@
+function test_suite = test_ChiCloneable
+initTestSuite;
+
+function [obj] = setup
+xvals = [1:10];
+yvals = [11:20];
+obj = ChiSpectrum(xvals,yvals);
+
+function test_clone_same(obj)
+
+cloned = clone(obj);
+assertEqual(obj, cloned);
+
+function test_clone_different(obj)
+
+cloned = clone(obj);
+changed = obj.subspectrumxvals(6,3);
+assertFalse(cloned == changed);
+

d2bb6245e8d0c6b2ae683ce9cddb657479cab91d
Alex Henderson <alex.henderson@manchester.ac.uk>
2014-11-27 14:16:35 +0000

    Added ChiSpectralCharacter.

---
+++ CHIToolbox/@ChiSpectralCharacter/ChiSpectralCharacter.m
@@ -0,0 +1,12 @@
+classdef ChiSpectralCharacter
+%ChiSpectralCharacter Abstract class to define spectral characteristics
+% Copyright (c) 2014 Alex Henderson (alex.henderson@manchester.ac.uk)
+    
+    properties
+    end
+    
+    methods
+    end
+    
+end
+

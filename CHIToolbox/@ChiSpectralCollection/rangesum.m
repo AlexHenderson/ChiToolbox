d7e18d84b2b397a6d50f1f36f83dd4548b1edca7
Alex Henderson <alex.henderson@manchester.ac.uk>
2017-07-12 18:27:02 +0100

    Renamed sumrange(idx) to rangesum(idx). Added help.

---
+++ CHIToolbox/@ChiSpectralCollection/rangesum.m
@@ -0,0 +1,41 @@
+function summed = rangesum(this,from,to)
+
+% rangesum  Calculates the sum of the spectral region. 
+%
+% Syntax
+%   summed = rangesum(from,to);
+%
+% Description
+%   summed = rangesum(from,to) calculates the sum of the spectra between
+%   from and to inclusive. The parameters from and to are in xaxis units.
+%   It returns a column vector of summed intensities.
+%
+% Copyright (c) 2017, Alex Henderson.
+% Licenced under the GNU General Public License (GPL) version 3.
+%
+% See also 
+%   rangesumidx ChiSpectralCollection.
+
+% Contact email: alex.henderson@manchester.ac.uk
+% Licenced under the GNU General Public License (GPL) version 3
+% http://www.gnu.org/copyleft/gpl.html
+% Other licensing options are available, please contact Alex for details
+% If you use this file in your work, please acknowledge the author(s) in
+% your publications. 
+
+% Version 1.0, July 2017
+% The latest version of this file is available on Bitbucket
+% https://bitbucket.org/AlexHenderson/chitoolbox
+
+
+    if ~exist('to','var')
+        % Only have a single x value, so only use that position
+        to = from;
+    end
+
+    % Determine the index values of the xvalue limits
+    fromidx = indexat(this, from);
+    toidx = indexat(this, to);
+%     this.history.add(['rangesum: from ', num2str(from), ' to ', num2str(to)]);
+    summed = rangesumidx(this,fromidx,toidx);
+end        
---
+++ CHIToolbox/@ChiSpectralCollection/rangesumidx.m
@@ -0,0 +1,55 @@
+function summed = rangesumidx(this,fromidx,toidx)
+
+% rangesumidx  Calculates the sum of the spectral region. 
+%
+% Syntax
+%   summed = rangesumidx(fromidx,toidx);
+%
+% Description
+%   summed = rangesumidx(fromidx,toidx) calculates the sum of the spectra
+%   between fromidx and toidx inclusive. The parameters fromidx and toidx
+%   are index values (not in xaxis units). It returns a column vector of
+%   summed intensities.
+%
+% Copyright (c) 2017, Alex Henderson.
+% Licenced under the GNU General Public License (GPL) version 3.
+%
+% See also 
+%   rangesum ChiSpectralCollection.
+
+% Contact email: alex.henderson@manchester.ac.uk
+% Licenced under the GNU General Public License (GPL) version 3
+% http://www.gnu.org/copyleft/gpl.html
+% Other licensing options are available, please contact Alex for details
+% If you use this file in your work, please acknowledge the author(s) in
+% your publications. 
+
+% Version 1.0, July 2017
+% The latest version of this file is available on Bitbucket
+% https://bitbucket.org/AlexHenderson/chitoolbox
+
+
+    if ~exist('toidx','var')
+        % Only have a single index value, so only use that position
+        toidx = fromidx;
+    end
+
+    % Check for out-of-range values
+    if (fromidx > this.channels) || (toidx > this.channels)
+        err = MException('CHI:ChiSpectralCollection:OutOfRange', ...
+            ['Requested range is too high. Max  = ', num2str(this.channels), '.']);
+        throw(err);
+    end            
+
+    if (fromidx < 1) || (toidx < 1)
+        err = MException('CHI:ChiSpectralCollection:OutOfRange', ...
+            'Requested range is invalid');
+        throw(err);
+    end            
+
+    % Swap if 'from' is higher than 'to'
+    [fromidx,toidx] = ChiForceIncreasing(fromidx,toidx);
+
+    summed = sum(this.data(:,fromidx:toidx),2);
+
+end        

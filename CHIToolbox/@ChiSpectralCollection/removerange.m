9b4d31de11ecdfbead393c71a5307f1acaf209f3
Alex Henderson <alex.henderson@manchester.ac.uk>
2017-07-12 17:56:22 +0100

    Modified to operate either in situ, or to generate a clone.

--- CHIToolbox/@ChiSpectralCollection/removerange.m
+++ CHIToolbox/@ChiSpectralCollection/removerange.m
@@ -1,13 +1,55 @@
-function output = removerange(this,from,to)
-% Remove a spectral range using x values
-% Copyright (c) 2014 Alex Henderson (alex.henderson@manchester.ac.uk)
+function varargout = removerange(this,from,to)
 
-    % Swap if 'from' is higher than 'to'
-    [from,to] = ChiForceIncreasing(from,to);
+% removerange  Removes a section of the spectrum (or spectra). 
+%
+% Syntax
+%   removerange(from,to);
+%   CSC = removerange(from,to);
+%
+% Description
+%   removerange(from,to) removes the region of the spectrum delimited by
+%   the values from and to (inclusive). The parameters from and to are in
+%   xaxis units (not index values). This version modifies the original
+%   object.
+%
+%   CSC = removerange(from,to) first creates a clone of the object, then
+%   removes the relevant portion of the spectrum from the clone. The
+%   original object is not modified.
+%
+% Copyright (c) 2017, Alex Henderson.
+% Licenced under the GNU General Public License (GPL) version 3.
+%
+% See also 
+%   removerangeidx ChiSpectralCollection.
 
-    % Determine the index values of the xvalue limits
-    fromidx = indexat(this, from);
-    toidx = indexat(this, to);
-    this.history.add(['removerange, from ', num2str(from), ' to ', num2str(to)]);
-    output = removerangeidx(this,fromidx,toidx);
+% Contact email: alex.henderson@manchester.ac.uk
+% Licenced under the GNU General Public License (GPL) version 3
+% http://www.gnu.org/copyleft/gpl.html
+% Other licensing options are available, please contact Alex for details
+% If you use this file in your work, please acknowledge the author(s) in
+% your publications. 
+
+% Version 1.0, July 2017
+% The latest version of this file is available on Bitbucket
+% https://bitbucket.org/AlexHenderson/agilent-file-formats
+
+
+% Swap if 'from' is higher than 'to'
+[from,to] = ChiForceIncreasing(from,to);
+
+% Determine the index values of the xvalue limits
+fromidx = indexat(this, from);
+toidx = indexat(this, to);
+
+if (nargout > 0)
+    % We are expecting to generate a modified clone of this object
+    varargout{1} = clone(this);
+    varargout{1}.history.add(['removerange: from ', num2str(from), ' to ', num2str(to)]);
+    removerangeidx(varargout{1},fromidx,toidx);
+else
+    % We are expecting to modified this object in situ
+    this.history.add(['removerange: from ', num2str(from), ' to ', num2str(to)]);
+    removerangeidx(this,fromidx,toidx);
+end
+
 end
--- CHIToolbox/@ChiSpectralCollection/removerangeidx.m
+++ CHIToolbox/@ChiSpectralCollection/removerangeidx.m
@@ -1,23 +1,58 @@
-function output = removerangeidx(this,fromidx,toidx)
-% Remove a spectral range using index values
-% usage:
-%   output = removerangeidx(fromidx,toidx);
-% Copyright (c) 2014 Alex Henderson (alex.henderson@manchester.ac.uk)
+function varargout = removerangeidx(this,fromidx,toidx)
 
-    % Swap if 'from' is higher than 'to'
-    [fromidx,toidx] = ChiForceIncreasing(fromidx,toidx);
+% removerangeidx  Removes a section of the spectrum (or spectra). 
+%
+% Syntax
+%   removerangeidx(from,to);
+%   CSC = removerangeidx(from,to);
+%
+% Description
+%   removerangeidx(from,to) removes the region of the spectrum delimited by
+%   the values from and to (inclusive). The parameters from and to are
+%   index values (not in xaxis units). This version modifies the original
+%   object.
+%
+%   CSC = removerangeidx(from,to) first creates a clone of the object, then
+%   removes the relevant portion of the spectrum from the clone. The
+%   original object is not modified.
+%
+% Copyright (c) 2017, Alex Henderson.
+% Licenced under the GNU General Public License (GPL) version 3.
+%
+% See also 
+%   removerange ChiSpectralCollection.
 
-    fromidx = fromidx - 1;
-    toidx = toidx + 1;
+% Contact email: alex.henderson@manchester.ac.uk
+% Licenced under the GNU General Public License (GPL) version 3
+% http://www.gnu.org/copyleft/gpl.html
+% Other licensing options are available, please contact Alex for details
+% If you use this file in your work, please acknowledge the author(s) in
+% your publications. 
 
-    tempdata = [this.data(:,1:fromidx),this.data(:,toidx:end)];
-    tempxvals = [this.xvals(1:fromidx),this.xvals(toidx:end)];
-    
-    % TODO: copy/clone?
-    output = clone(this);
-    output.xvals = tempxvals;
-    output.data = tempdata;
-%     output = ChiSpectralCollection(tempxvals,tempdata,this.reversex,this.xlabel,this.ylabel);
-    output.history.add(['removerangefromindexvals, from ', num2str(fromidx), ' to ', num2str(toidx)]);
-    this.history.add(['removerangefromindexvals, from ', num2str(fromidx), ' to ', num2str(toidx)]);
+% Version 1.0, July 2017
+% The latest version of this file is available on Bitbucket
+% https://bitbucket.org/AlexHenderson/agilent-file-formats
+
+
+% Swap if 'from' is higher than 'to'
+[fromidx,toidx] = ChiForceIncreasing(fromidx,toidx);
+
+% Generating output by saving spectral pieces, rather than deleting the
+% unwanted part, so need to step the limits outwards.
+fromidx = fromidx - 1;
+toidx = toidx + 1;
+
+if (nargout > 0)
+    % We are expecting to generate a modified clone of this object
+    varargout{1} = clone(this);
+    varargout{1}.xvals = [varargout{1}.xvals(1:fromidx),varargout{1}.xvals(toidx:end)];
+    varargout{1}.data = [varargout{1}.data(:,1:fromidx),varargout{1}.data(:,toidx:end)];
+    varargout{1}.history.add(['removerangefromindexvals: from ', num2str(fromidx), ' to ', num2str(toidx)]);    
+else
+    % We are expecting to modified this object in situ
+    this.xvals = [this.xvals(1:fromidx),this.xvals(toidx:end)];
+    this.data = [this.data(:,1:fromidx),this.data(:,toidx:end)];
+    this.history.add(['removerangefromindexvals: from ', num2str(fromidx), ' to ', num2str(toidx)]);
+end
+
 end

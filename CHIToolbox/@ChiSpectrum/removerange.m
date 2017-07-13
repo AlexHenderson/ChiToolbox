c88d9108c4c02fd52e45ea60cef9b1b733d5fb3f
Alex Henderson <alex.henderson@manchester.ac.uk>
2017-07-13 15:13:19 +0100

    Added rangesum(idx) and removerange(idx) functions.

---
+++ CHIToolbox/@ChiSpectrum/removerange.m
@@ -0,0 +1,55 @@
+function varargout = removerange(this,from,to)
+
+% removerange  Removes a section of the spectrum. 
+%
+% Syntax
+%   removerange(from,to);
+%   CS = removerange(from,to);
+%
+% Description
+%   removerange(from,to) removes the region of the spectrum delimited by
+%   the values from and to (inclusive). The parameters from and to are in
+%   xaxis units (not index values). This version modifies the original
+%   object.
+%
+%   CS = removerange(from,to) first creates a clone of the object, then
+%   removes the relevant portion of the spectrum from the clone. The
+%   original object is not modified.
+%
+% Copyright (c) 2017, Alex Henderson.
+% Licenced under the GNU General Public License (GPL) version 3.
+%
+% See also 
+%   removerangeidx ChiSpectrum.
+
+% Contact email: alex.henderson@manchester.ac.uk
+% Licenced under the GNU General Public License (GPL) version 3
+% http://www.gnu.org/copyleft/gpl.html
+% Other licensing options are available, please contact Alex for details
+% If you use this file in your work, please acknowledge the author(s) in
+% your publications. 
+
+% Version 1.0, July 2017
+% The latest version of this file is available on Bitbucket
+% https://bitbucket.org/AlexHenderson/chitoolbox
+
+
+% Swap if 'from' is higher than 'to'
+[from,to] = ChiForceIncreasing(from,to);
+
+% Determine the index values of the xvalue limits
+fromidx = indexat(this, from);
+toidx = indexat(this, to);
+
+if (nargout > 0)
+    % We are expecting to generate a modified clone of this object
+    varargout{1} = clone(this);
+    varargout{1}.history.add(['removerange: from ', num2str(from), ' to ', num2str(to)]);
+    removerangeidx(varargout{1},fromidx,toidx);
+else
+    % We are expecting to modified this object in situ
+    this.history.add(['removerange: from ', num2str(from), ' to ', num2str(to)]);
+    removerangeidx(this,fromidx,toidx);
+end
+
+end
---
+++ CHIToolbox/@ChiSpectrum/removerangeidx.m
@@ -0,0 +1,58 @@
+function varargout = removerangeidx(this,fromidx,toidx)
+
+% removerangeidx  Removes a section of the spectrum. 
+%
+% Syntax
+%   removerangeidx(fromidx,toidx);
+%   CS = removerangeidx(fromidx,toidx);
+%
+% Description
+%   removerangeidx(fromidx,toidx) removes the region of the spectrum
+%   delimited by the values fromidx and toidx (inclusive). The parameters
+%   fromidx and toidx are index values (not in xaxis units). This version
+%   modifies the original object.
+%
+%   CS = removerangeidx(fromidx,toidx) first creates a clone of the
+%   object, then removes the relevant portion of the spectrum from the
+%   clone. The original object is not modified.
+%
+% Copyright (c) 2017, Alex Henderson.
+% Licenced under the GNU General Public License (GPL) version 3.
+%
+% See also 
+%   removerange ChiSpectrum.
+
+% Contact email: alex.henderson@manchester.ac.uk
+% Licenced under the GNU General Public License (GPL) version 3
+% http://www.gnu.org/copyleft/gpl.html
+% Other licensing options are available, please contact Alex for details
+% If you use this file in your work, please acknowledge the author(s) in
+% your publications. 
+
+% Version 1.0, July 2017
+% The latest version of this file is available on Bitbucket
+% https://bitbucket.org/AlexHenderson/chitoolbox
+
+
+% Swap if 'from' is higher than 'to'
+[fromidx,toidx] = ChiForceIncreasing(fromidx,toidx);
+
+% Generating output by saving spectral pieces, rather than deleting the
+% unwanted part, so need to step the limits outwards.
+fromidx = fromidx - 1;
+toidx = toidx + 1;
+
+if (nargout > 0)
+    % We are expecting to generate a modified clone of this object
+    varargout{1} = clone(this);
+    varargout{1}.xvals = [varargout{1}.xvals(1:fromidx),varargout{1}.xvals(toidx:end)];
+    varargout{1}.data = [varargout{1}.data(:,1:fromidx),varargout{1}.data(:,toidx:end)];
+    varargout{1}.history.add(['removerangeidx: from ', num2str(fromidx), ' to ', num2str(toidx)]);    
+else
+    % We are expecting to modified this object in situ
+    this.xvals = [this.xvals(1:fromidx),this.xvals(toidx:end)];
+    this.data = [this.data(:,1:fromidx),this.data(:,toidx:end)];
+    this.history.add(['removerangeidx: from ', num2str(fromidx), ' to ', num2str(toidx)]);
+end
+
+end

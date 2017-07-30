cf01e6e2428d7a89cdc8dae5694c8bef21132306
Alex Henderson <alex.henderson@manchester.ac.uk>
2017-07-30 20:13:56 +0100

    Added help information.

--- CHIToolbox/@ChiSpectralCollection/pca.m
+++ CHIToolbox/@ChiSpectralCollection/pca.m
@@ -1,7 +1,33 @@
-function output = pca(this)
-% pca
+function pcaresult = pca(this)
 
-output = ChiSpectralPCA(this);
+% pca Principal Components Analysis
+%
+% Syntax
+%   pcaresult = pca();
+%
+% Description
+%   pcaresult = pca() performs principal components analysis on the spectra
+%   in this collection. The data is mean centered internally. The output is
+%   stored in a ChiSpectralPCAOutcome object.
+%
+% Copyright (c) 2017, Alex Henderson.
+% Licenced under the GNU General Public License (GPL) version 3.
+%
+% See also 
+%   princomp pca ChiSpectralPCAOutcome ChiSpectralCollection.
+
+% Contact email: alex.henderson@manchester.ac.uk
+% Licenced under the GNU General Public License (GPL) version 3
+% http://www.gnu.org/copyleft/gpl.html
+% Other licensing options are available, please contact Alex for details
+% If you use this file in your work, please acknowledge the author(s) in
+% your publications. 
+
+% Version 1.0, July 2017
+% The latest version of this file is available on Bitbucket
+% https://bitbucket.org/AlexHenderson/chitoolbox
+
+
+pcaresult = ChiSpectralPCA(this);
 
 end
-

ad3be115e24f0a2bd7c7fa895b41afe1141cee24
Alex Henderson <alex.henderson@manchester.ac.uk>
2017-08-01 12:30:56 +0100

    Added convenience function to remove CO regions from spectra (2250 - 2450 wavenumbers).

---
+++ CHIToolbox/@ChiSpectralCollection/removeco.m
@@ -0,0 +1,37 @@
+function varargout = removeco(this,varargin)
+
+% removeco  Removes the CO region from the spectra (2250-2450 cm^{-1}). 
+%
+% Syntax
+%   removeco();
+%   modified = removeco();
+%
+% Description
+%   removeco() removes the CO region of the spectra delimited 2250 and 2450
+%   wavenumbers. This version modifies the original object.
+%
+%   modified = removeco() first creates a clone of the object, then removes
+%   the CO region of the spectra from the clone. The original object is not
+%   modified.
+%
+% Copyright (c) 2017, Alex Henderson.
+% Licenced under the GNU General Public License (GPL) version 3.
+%
+% See also 
+%   removerange ChiSpectralCollection.
+
+% Contact email: alex.henderson@manchester.ac.uk
+% Licenced under the GNU General Public License (GPL) version 3
+% http://www.gnu.org/copyleft/gpl.html
+% Other licensing options are available, please contact Alex for details
+% If you use this file in your work, please acknowledge the author(s) in
+% your publications. 
+
+% Version 1.0, August 2017
+% The latest version of this file is available on Bitbucket
+% https://bitbucket.org/AlexHenderson/chitoolbox
+
+
+varargout{:} = removerange(this,2250,2450);
+
+end % function removeco

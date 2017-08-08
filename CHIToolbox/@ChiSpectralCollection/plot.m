1c01f733d45fb55eac6d194394b6694e66aaf865
Alex Henderson <alex.henderson@manchester.ac.uk>
2017-08-08 15:02:44 +0100

    Separated plotspectrum from plotspectra for simplicity.

--- CHIToolbox/@ChiSpectralCollection/plot.m
+++ CHIToolbox/@ChiSpectralCollection/plot.m
@@ -54,6 +54,6 @@ function plot(this,varargin)
 % Passing the actual plotting functionality off to a separate function to
 % co-locate the feature. 
 
-    utilities.plotspectrum(this,varargin{:});
+    utilities.plotspectra(this,varargin{:});
     
 end

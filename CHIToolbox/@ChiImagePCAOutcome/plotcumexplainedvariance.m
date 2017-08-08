937cba868a9ac3f660d7fdab9654af006e1e09ec
Alex Henderson <alex.henderson@manchester.ac.uk>
2017-08-08 15:47:19 +0100

    Modified to generate a new figure by default.

--- CHIToolbox/@ChiImagePCAOutcome/plotcumexplainedvariance.m
+++ CHIToolbox/@ChiImagePCAOutcome/plotcumexplainedvariance.m
@@ -1,49 +1,84 @@
-function plotcumexplainedvariance(this,limitpcs)
+function plotcumexplainedvariance(this,limitpcs,varargin)
 
 % plotcumexplainedvariance Plots cumulative percentage explained variance
-% usage:
-%     plotcumexplainedvariance(this);
-%     plotcumexplainedvariance(this, limitpcs);
 %
-% where:
-%   limitpcs - (optional) by default the function plots the first 20 pcs. If
-%       'limitpcs' is present then this determines the number of principal
-%       components displayed
+% Syntax
+%   plotcumexplainedvariance();
+%   plotcumexplainedvariance(limitpcs);
+%   plotcumexplainedvariance(____,'nofig');
 %
-%   Copyright (c) 2015-2017, Alex Henderson 
-%   Contact email: alex.henderson@manchester.ac.uk
-%   Licenced under the GNU General Public License (GPL) version 3
-%   http://www.gnu.org/copyleft/gpl.html
-%   Other licensing options are available, please contact Alex for details
-%   If you use this file in your work, please acknowledge the author(s) in
-%   your publications. 
+% Description
+%   plotcumexplainedvariance() creates a 2-D plot of the cumulative
+%   percentage variance explained by each principal component. Up to 20
+%   principal components are plotted by default. A line indicates the
+%   position of 95% explained variance. A new figure window is generated.
 %
-%   version 2.0 July 2017
+%   plotcumexplainedvariance(limitpcs) plots the cumulative percentage
+%   explained variance with limitpcs values being displayed.
+%
+%   plotcumexplainedvariance(____,'nofig') plots the data in the currently
+%   active figure window, or creates a new figure if none is available.
+%
+%   Other parameters can be applied to customise the plot. See the MATLAB
+%   plot function for more details. 
+%
+% Copyright (c) 2017, Alex Henderson.
+% Licenced under the GNU General Public License (GPL) version 3.
+%
+% See also 
+%   plot plotscores plotloading plotexplainedvariance
+%   ChiImagePCAOutcome ChiImage.
 
-%   version 2.0 July 2017 Alex Henderson
-%   Modified as a class function
-%   version 1.0 June 2015 Alex Henderson
-%   initial release
+% Contact email: alex.henderson@manchester.ac.uk
+% Licenced under the GNU General Public License (GPL) version 3
+% http://www.gnu.org/copyleft/gpl.html
+% Other licensing options are available, please contact Alex for details
+% If you use this file in your work, please acknowledge the author(s) in
+% your publications. 
 
-% window_title = 'Cumulative percentage explained variance';
-% figure_handle = figure('Name',window_title,'NumberTitle','off');
+% Version 1.0, July 2017
+% The latest version of this file is available on Bitbucket
+% https://bitbucket.org/AlexHenderson/chitoolbox
 
-cumpcexplained = cumsum(this.explained);
+errorcode = 'CHI:ChiSpectralPCAOutcome';
+errormessagestub = 'Requested principal component is out of range. Max PCs = ';
 
-if (exist('limitpcs','var'))
-    plot(cumpcexplained(1:limitpcs), 'o-');
-else
-    plot(cumpcexplained(1:20), 'o-');
+if exist('limitpcs','var')
+    if ((limitpcs > this.pcs) || (limitpcs < 1))
+        err = MException([errorcode,':OutOfRange'], ...
+            [errormessagestub, num2str(this.numpcs), '.']);
+        throw(err);
+    end
 end
 
-    % Draw line indicating 95% cumulative explained variance
-    axiscolour = 'k';
-    hold on;
-    limits = axis;
-    xmin = limits(1,1);
-    xmax = limits(1,2);
-    plot([xmin,xmax], [95,95], axiscolour);
-    hold off;
+argposition = find(cellfun(@(x) strcmpi(x, 'nofig') , varargin));
+if argposition
+    % Remove the parameter from the argument list
+    varargin(argposition) = [];
+else
+    % No 'nofig' found so create a new figure
+    windowtitle = 'Cumulative percentage explained variance';
+    figure('Name',windowtitle,'NumberTitle','off');
+end
+
+cumexplained = cumsum(this.explained);
+
+if exist('limitpcs','var')
+    plot(cumexplained(1:limitpcs),'o-',varargin{:});
+else
+    maxpcs = min(20,length(cumexplained));
+    plot(cumexplained(1:maxpcs),'o-',varargin{:});
+end
+
+% Draw line indicating 95% cumulative explained variance
+axiscolour = 'k';
+hold on;
+limits = axis;
+xmin = limits(1,1);
+xmax = limits(1,2);
+plot([xmin,xmax], [95,95], axiscolour);
+hold off;
+
 
 xlabel('principal component number');
 ylabel('cumulative percentage explained variance');

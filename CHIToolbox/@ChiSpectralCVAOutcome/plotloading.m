da5a1e39b611c5689a656f50a4747d918054f712
Alex Henderson <alex.henderson@manchester.ac.uk>
2017-08-08 15:48:00 +0100

    Added help information.

--- CHIToolbox/@ChiSpectralCVAOutcome/plotloading.m
+++ CHIToolbox/@ChiSpectralCVAOutcome/plotloading.m
@@ -1,5 +1,40 @@
 function plotloading(this,cv,varargin)
-% plotcvloading
+
+% plotloading  Plots the canonical variate loading of your choice. 
+%
+% Syntax
+%   plotloading(cv);
+%   plotloading(cv,'nofig');
+%
+% Description
+%   plotloading(cv) creates a 2-D bar chart of the canonical variate cv in
+%   a new figure window.
+%
+%   plotloading(cv,'nofig') plots the loading in the currently active
+%   figure window, or creates a new figure if none is available.
+%
+%   Other parameters can be applied to customise the plot. See the MATLAB
+%   bar function for more details. 
+%
+% Copyright (c) 2017, Alex Henderson.
+% Licenced under the GNU General Public License (GPL) version 3.
+%
+% See also 
+%   bar plotscores plotexplainedvariance plotcumexplainedvariance
+%   plotpcscores plotpcloading plotpcexplainedvariance
+%   plotpccumexplainedvariance ChiSpectralCVAOutcome ChiSpectralCollection.
+
+% Contact email: alex.henderson@manchester.ac.uk
+% Licenced under the GNU General Public License (GPL) version 3
+% http://www.gnu.org/copyleft/gpl.html
+% Other licensing options are available, please contact Alex for details
+% If you use this file in your work, please acknowledge the author(s) in
+% your publications. 
+
+% Version 1.0, July 2017
+% The latest version of this file is available on Bitbucket
+% https://bitbucket.org/AlexHenderson/chitoolbox
+
 
     titlestub = 'Loading on canonical variate ';
     windowtitlestub = titlestub;

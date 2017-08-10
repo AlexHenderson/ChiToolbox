14d3464db1886014a61a22b0a92c0bdea0ad560c
Alex Henderson <alex.henderson@manchester.ac.uk>
2017-08-10 13:37:53 +0100

    Added management of metadata.

---
+++ CHIToolbox/@ChiMetadata/ChiMetadata.m
@@ -0,0 +1,132 @@
+classdef ChiMetadata
+
+% ChiMetadata Metadata file reader
+
+    properties
+        title
+        owner
+        datapath
+        filenames
+        acquisitiondate
+        metadatafile
+        filter
+        numparameters
+        membershipnames    
+        safemembershipnames
+        parametertypes    
+        classmemberships        
+    end
+    
+    properties (Dependent = true)
+        fullfilenames
+        numfiles
+    end
+    
+    methods
+        function this = ChiMetadata(varargin)
+            if (nargin > 0) % Support calling with 0 arguments
+            
+                met = this.metadatareader(varargin{:});
+
+                this.title = met.title;
+                this.owner = met.owner;
+                this.datapath = met.dataPath;
+                this.filenames = met.filenames;
+                this.acquisitiondate = met.acquisitionDate;
+                this.metadatafile = met.metadataFile;
+                this.filter = met.filter;
+                this.numparameters = met.numParameters;
+                this.membershipnames = met.originalParameterNames;
+                this.parametertypes = met.parameterTypes;
+                this.safemembershipnames = met.safeParameterNames;
+
+                this.classmemberships = cell(this.numparameters,1);
+                for i = 1:this.numparameters
+                    this.classmemberships{i} = ChiClassMembership(this.membershipnames{i},met.parameters{i});
+                end
+            end
+        end
+        
+        function x = clone(this)
+            x = feval(class(this));
+ 
+            x.title = this.title;
+            x.owner = this.owner;
+            x.datapath = this.datapath;
+            x.filenames = this.filenames;
+            x.acquisitiondate = this.acquisitiondate;
+            x.metadatafile = this.metadatafile;
+            x.filter = this.filter;
+            x.numparameters = this.numparameters;
+            x.membershipnames = this.membershipnames;
+            x.parametertypes = this.parametertypes;
+            x.safemembershipnames = this.safemembershipnames;
+            
+            x.classmemberships = cell(this.numparameters,1);
+            for i = 1:this.numparameters
+                % Deep copy required
+                x.classmemberships{i} = this.classmemberships{i}.clone();
+            end
+            
+        end            
+
+%         function duplicate = clone(this)
+%             % from https://uk.mathworks.com/matlabcentral/newsreader/view_thread/257925            
+%             % Instantiate new object of the same class.
+%             
+%             duplicate = feval(class(this));
+%  
+%             % Copy all non-hidden properties.
+%             p = properties(this);
+%             for i = 1:length(p)
+%                 % Need a (MUCH BETTER) way of isolating/detecting dependent
+%                 % properties
+%                 if ~(strcmpi(p{i},'fullfilenames') || strcmpi(p{i},'numfiles'))
+%                     duplicate.(p{i}) = this.(p{i});
+%                 end
+%             end
+%         end            
+        
+        function m = membership(this,which)
+            
+            if isnumeric(which)
+                m = this.classmemberships{which};
+            else
+                if ischar(which)
+                    for i = 1:this.numparameters
+                        if strcmpi(this.membershipnames{i},which)
+                            m = this.classmemberships{i};
+                            break
+                        end
+                            name = evalin('caller','inputname(1)');
+                            err = MException('CHI:ChiMetadata:UnknownInput', ...
+                                ['Membership name ''', which, ''' not recognised. Use ''', name, '.membershipnames()'' to view options.']);
+                            throw(err);
+                    end
+                else
+                    err = MException('CHI:ChiMetadata:UnknownInput', ...
+                        'Enter the name of the membership as a string, or its position number.');
+                    throw(err);
+                end
+            end
+        end
+        
+        function [nm] = objectname(~)
+            nm = inputname(1);
+        end        
+        
+    end
+
+    methods % Dependent
+
+        function fullfilenames = get.fullfilenames(this)
+            fullfilenames = fullfile(this.datapath,this.filenames);
+        end
+        
+        function numfiles = get.numfiles(this)
+            numfiles = size(this.filenames,1);
+        end        
+        
+    end
+    
+end

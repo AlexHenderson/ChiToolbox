1e8a24bbb6a60de12ca6c14ba6fee0fc2958d541
Alex Henderson <alex.henderson@manchester.ac.uk>
2017-08-25 15:05:20 +0100

    Added initiation date and time.

--- CHIToolbox/@ChiLogger/ChiLogger.m
+++ CHIToolbox/@ChiLogger/ChiLogger.m
@@ -1,26 +1,26 @@
 classdef ChiLogger < handle
-% ChiLogger Logs changes to objects
-% Copyright (c) 2014 Alex Henderson (alex.henderson@manchester.ac.uk)
+
+% ChiLogger  Records changes to objects
+% Copyright (c) 2017 Alex Henderson (alex.henderson@manchester.ac.uk)
     
     properties
         log;
     end
     
     methods
+        % ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
         function this = ChiLogger()
-            % Create an instance of ChiLogger
             this.log = cell(1);
-%             if (nargin > 0) % Support calling with 0 arguments
-%                 this.log = cell(1);
-%             end
+            this.log{1} = ['Created: ', datestr(now)]; 
         end
         
+        % ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
         function output = clone(this)
-            % Create an instance of ChiLogger
             output = ChiLogger();
             output.log = this.log;
         end
         
+        % ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     end
     
 end

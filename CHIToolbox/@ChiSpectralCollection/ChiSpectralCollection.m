69d59e9ba3b929572c9cfc290f7a88f2f0462c0c
Alex Henderson <alex.henderson@manchester.ac.uk>
2017-08-25 15:13:51 +0100

    Changed the constructor and cloning methodology.

--- CHIToolbox/@ChiSpectralCollection/ChiSpectralCollection.m
+++ CHIToolbox/@ChiSpectralCollection/ChiSpectralCollection.m
@@ -1,15 +1,55 @@
 classdef ChiSpectralCollection < ChiAbstractSpectralCollection
-% ChiSpectralCollection Storage class for hyperspectral images
-% Copyright (c) 2014 Alex Henderson (alex.henderson@manchester.ac.uk)
+
     
-% ToDo: We need a mechanism of adding collections of spectra together. A
-% collection could simply be a single spectrum or ChiSpectrum. We could
-% interpolate these in the same manner as BiotofSpectrum. 
+% ChiSpectralCollection  Storage class for spectra
+%
+% Syntax
+%   collection = ChiSpectralCollection();
+%   collection = ChiSpectralCollection(wavenumbers,data);
+%   collection = ChiSpectralCollection(wavenumbers,data,reversex);
+%   collection = ChiSpectralCollection(wavenumbers,data,reversex,xlabel,ylabel);
+%   collection = ChiSpectralCollection(ChiSpectrum);
+% 
+% Description
+%   collection = ChiSpectralCollection() creates an empty spectral
+%   collection.
+%
+%   collection = ChiSpectralCollection(wavenumbers,data) creates a spectral
+%   collection with a spectrum 
 
 
-    % matlab.mixin.Copyable only for >R2011a
-    % Want compatibility with R2009a
-    
+
+
+
+%   object using default values for reversex, xlabel and ylabel.
+%
+%   IRSpectrum = ChiIRSpectrum(ChiSpectrum) creates an IR spectrum object
+%   from a ChiSpectrum object using default values for reversex, xlabel and
+%   ylabel. No check is made to determine if the ChiSpectrum object
+%   contains valid IR data.
+% 
+%   Default values are reversex = true (wavenumbers are plotted in
+%   descending order), xlabel = 'wavenumber (cm^{-1})' and ylabel =
+%   'absorbance'.
+%
+% Copyright (c) 2017, Alex Henderson.
+% Licenced under the GNU General Public License (GPL) version 3.
+%
+% See also 
+%   ChiSpectrum ChiIRSpectralCollection ChiSpectralCollection.
+
+% Contact email: alex.henderson@manchester.ac.uk
+% Licenced under the GNU General Public License (GPL) version 3
+% http://www.gnu.org/copyleft/gpl.html
+% Other licensing options are available, please contact Alex for details
+% If you use this file in your work, please acknowledge the author(s) in
+% your publications. 
+
+% Version 1.0, August 2017
+% The latest version of this file is available on Bitbucket
+% https://bitbucket.org/AlexHenderson/chitoolbox
+
+
     % ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     properties  
         xvals  % abscissa as a row vector
@@ -33,46 +73,96 @@ classdef ChiSpectralCollection < ChiAbstractSpectralCollection
     methods
         % ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
         % Constructor
-        function this = ChiSpectralCollection(xvals,data,reversex,xlabel,ylabel,varargin)
-            % Create an instance of ChiImage with given parameters
-
-            this.history = ChiLogger();
-
-            if (nargin > 0) % Support calling with 0 arguments
-                this.xvals = xvals;
-                this.data = data;
-
-                % Force x-values to row vector
-                this.xvals = ChiForceToRow(this.xvals);
-
-                % Force y-values to row vectors
-                [rows,cols] = size(this.data);
-                if (rows == cols)
-                    utilities.warningnobacktrace('Data matrix is square. Assuming spectra are in rows.');
-                else
-                    if (rows == length(this.xvals))
-                        this.data = this.data';
+        
+        function this = ChiSpectralCollection(varargin)
+            
+            switch nargin
+                case 0
+                    this.history = ChiLogger();
+                case 1
+                    if isa(varargin{1},'ChiSpectrum')
+                        s = varargin{1};
+                        this.xvals = s.xvals;
+                        this.data = s.data;
+                        this.reversex = s.reversex;
+                        this.xlabel = s.xlabel;
+                        this.ylabel = s.ylabel;
+                        this.classmembership = ChiClassMembership();
+                        this.history = ChiLogger();
+                        this.history.add(['Generated from a ', class(s), '. Filename: ', s.filename]);                        
+                    else
+                        err = MException('CHI:ChiSpectralCollection:InputError', ...
+                            'Input not understood. Try creating a ChiSpectrum and using that as input.');
+                        throw(err);
                     end
-                end                      
-
-                % Set other parameters if available
-                if (nargin > 2)
-                    this.reversex = reversex;
-                    if (nargin > 3)
-                        this.xlabel = xlabel;
-                        this.ylabel = ylabel;
-                    end
-                end
-            end 
+                otherwise
+                    err = MException('CHI:ChiSpectralCollection:InputError', ...
+                        'Input not understood. Try creating a ChiSpectrum and using that as input.');
+                    throw(err);
+            end
+                        
         end
+        
+        
+%         function this = ChiSpectralCollection(xvals,data,reversex,xlabel,ylabel,varargin)
+%             % Create an instance of ChiSpectralCollection with given parameters
+% 
+%             this.history = ChiLogger();
+% 
+%             if (nargin > 0) % Support calling with 0 arguments
+%                 this.xvals = xvals;
+%                 this.data = data;
+% 
+%                 % Force x-values to row vector
+%                 this.xvals = ChiForceToRow(this.xvals);
+% 
+%                 % Force y-values to row vectors
+%                 [rows,cols] = size(this.data);
+%                 if (rows == cols)
+%                     utilities.warningnobacktrace('Data matrix is square. Assuming spectra are in rows.');
+%                 else
+%                     if (rows == length(this.xvals))
+%                         this.data = this.data';
+%                     end
+%                 end                      
+% 
+%                 % Set other parameters if available
+%                 if (nargin > 2)
+%                     this.reversex = reversex;
+%                     if (nargin > 3)
+%                         this.xlabel = xlabel;
+%                         this.ylabel = ylabel;
+%                     end
+%                 end
+%             end 
+%         end
 
         % ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
         % Make a copy of this image
-        function output = clone(this)
-            % Make a copy of this image
-            output = ChiSpectralCollection(this.xvals,this.data,this.reversex,this.xlabel,this.ylabel);
-            output.classmembership = this.classmembership;
-            output.history = this.history.clone();
+%         function output = clone(this)
+%             % Make a copy of this image
+%             output = ChiSpectralCollection(this.xvals,this.data,this.reversex,this.xlabel,this.ylabel);
+%             output.classmembership = this.classmembership;
+%             output.history = this.history.clone();
+%         end
+
+        function obj = clone(this)
+            
+            % ToDo: There's got to be a better way!!
+            % http://undocumentedmatlab.com/blog/general-use-object-copy
+            
+            obj = ChiSpectralCollection();
+            
+            obj.xvals = this.xvals;
+            obj.data = this.data;
+            obj.reversex = this.xvals;
+            obj.xlabel = this.xlabel;
+            obj.ylabel = this.ylabel;
+            if ~isempty(this.classmembership)
+                obj.classmembership = this.classmembership.clone();
+            end
+            obj.history = this.history.clone();
+            obj.history.add('Cloned');
         end
 
         % ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

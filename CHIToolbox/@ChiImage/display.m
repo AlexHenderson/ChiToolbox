31ccc8321a5c389d8a8a907758558fc562fcda05
Alex Henderson <alex.henderson@manchester.ac.uk>
2017-08-30 17:40:43 +0100

    Changed display to generate a new figure on demand.

--- CHIToolbox/@ChiImage/display.m
+++ CHIToolbox/@ChiImage/display.m
@@ -1,6 +1,16 @@
 function display(this,varargin)
 % display Basic display function
 
+%% Do we need a new figure?
+argposition = find(cellfun(@(x) strcmpi(x, 'nofig') , varargin));
+if argposition
+    % Remove the parameter from the argument list
+    varargin(argposition) = [];
+else
+    % No 'nofig' found so create a new figure
+    figure;
+end
+
     if ~isempty(this.data)
         imagesc(this.totalimage(),varargin{:});
         if exist('parula.m','file')

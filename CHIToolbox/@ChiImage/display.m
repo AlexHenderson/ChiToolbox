55229b2f958d6ea85c3b1a84b3c034e5fb0dbdd4
Alex Henderson <alex.henderson@manchester.ac.uk>
2018-01-10 12:42:35 +0000

    Modified display to generate a new figure if required. Capture the figure handle as an output.

--- CHIToolbox/@ChiImage/display.m
+++ CHIToolbox/@ChiImage/display.m
@@ -1,24 +1,39 @@
-function display(this,varargin)
+function varargout = display(this,varargin)
+
 % display Basic display function
 
-%% Do we need a new figure?
-argposition = find(cellfun(@(x) strcmpi(x, 'nofig') , varargin));
-if argposition
-    % Remove the parameter from the argument list
-    varargin(argposition) = [];
-else
-    % No 'nofig' found so create a new figure
-    figure;
-end
+if ~isempty(this.data)
 
-    if ~isempty(this.data)
-        imagesc(this.totalimage(),varargin{:});
-        if exist('parula.m','file')
-            colormap(parula);
-        else
-            colormap(ChiSequentialColormap());
-        end
-        axis image;
-        axis off;
+    % Do we need a new figure?
+    argposition = find(cellfun(@(x) strcmpi(x, 'nofig') , varargin));
+    if argposition
+        % Remove the parameter from the argument list
+        varargin(argposition) = [];
+    else
+        % No 'nofig' found so create a new figure
+        figure;
     end
+
+    % Has the user asked for the figure handle?
+    if nargout
+        varargout{1} = gcf();
+    end
+    
+    % Generate the image
+    imagesc(this.totalimage(),varargin{:});
+    if exist('parula.m','file')
+        colormap(parula);
+    else
+        colormap(ChiSequentialColormap());
+    end
+    axis image;
+    axis off;
+    
+else
+    err = MException(['CHI:',mfilename,':DisplayError'], ...
+        'No data to display.');
+    throw(err);
+end
+    
+    
 end

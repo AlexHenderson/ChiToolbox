1c0e0ff16d03bd817cd396534d1780747579edf1
Alex Henderson <alex.henderson@manchester.ac.uk>
2018-03-01 18:52:27 +0000

    Converted some properties to dependent versions.

--- CHIToolbox/@ChiKFoldOutcome/ChiKFoldOutcome.m
+++ CHIToolbox/@ChiKFoldOutcome/ChiKFoldOutcome.m
@@ -3,34 +3,64 @@ classdef ChiKFoldOutcome < handle
     %   Detailed explanation goes here
     
     properties
-        numfolds;
         folds;
-        pcs;
         history;        
+    end
+    
+    properties (Dependent = true)
+        numfolds;
+        numcvs;
+        numpcs;
+        classnames;
     end
     
     methods
         %% Constructor
-        function this = ChiKFoldOutcome(numfolds,folds,pcs)
+        function this = ChiKFoldOutcome(folds)
             % Create an instance of ChiSpectralCVAOutcome with given parameters
             
             this.history = ChiLogger();
             if (nargin > 0) % Support calling with 0 arguments
                 
-                this.numfolds = numfolds;
                 this.folds = folds;
-                this.pcs = pcs;
             end 
         end
         
         %% clone : Make a copy 
         function output = clone(this)
             % Make a copy 
-            output = ChiKFoldOutcome(this.numfolds,this.folds,this.pcs);
+            output = ChiKFoldOutcome(this.folds);
             output.history = this.history;
         end
         
+    end
+    
+    methods
+        
+        %% numfolds : Number of folds
+        function numfolds = get.numfolds(this)
+            % numfolds : Number of folds
+            numfolds = size(this.folds,1);
+        end
 
+        %% numcvs : Number of canonical variates
+        function numcvs = get.numcvs(this)
+            % numcvs : Number of canonical variates
+            numcvs = this.folds{1}.pccva.numcvs;
+        end
+
+        %% numpcs : Number of principal components
+        function numpcs = get.numpcs(this)
+            % numcvs : Number of canonical variates
+            numpcs = this.folds{1}.pccva.pcs;
+        end
+
+        %% classnames : Unique class names
+        function classnames = get.classnames(this)
+            % classnames : Unique class names
+            classnames = this.folds{1}.pccva.pca.classmembership.uniquelabels;
+        end
+        
     end
     
 end

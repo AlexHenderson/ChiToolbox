334cf08ff9542eca22ca24199fbd96df6df2573c
Alex Henderson <alex.henderson@manchester.ac.uk>
2018-03-11 14:45:42 +0000

    Changed colour scheme.

--- CHIToolbox/todo.m
+++ CHIToolbox/todo.m
@@ -1,23 +1,32 @@
 % ToDo
 
+Cloning...
+    https://uk.mathworks.com/matlabcentral/fileexchange/22965-clone-handle-object-using-matlab-oop?s_cid=ME_prod_FX
+use  isa(beads,'handle') to get the inheritance of the object
+feval is useful to crfeate a new empty class
+
+New version of mixin uses copyelement
+edit matlab.mixin.Copyable
+
+
 %% ADD...
 
 % PLS
-% CVA
-% PCCVA
 % kfoldpccva
 % 
-% remove CO
-% Remove regions
-% keep regions
-% 
-% measurearea
-% 
-% read a collection of spectra
-% 
 % first and second derivative
+% pca/princomp from Octave
+% better metadataReader -> ChiClassMembership
 
-% error ellipse
+%  Selection of which class membership to use, if there are more than one
+%  present
+
+% Chi -> RMieS -> Chi
+
+% Absorbance and transmittance
+% Multiple spc files that are in different y-units
+
+% plots with blank spaces where regions removed
 
 %% ALSO DO...
 
@@ -46,45 +55,42 @@
 % other normalisations (other than vectornorm)
 
 
-
-
-##Data types ##
-
-Need to be able to handle the following data types: 
-
-1D (spectrum)
-2D (picture)
-3D (hyperspectral image)
-4D (hyperspectral 3D image)
-
-Spatially modified
-2D with pixels missing
-3D with pixels missing
-4D with pixels missing
-
-Spectrally modified
-1D sparse or with spectral ranges removed
-3D sparse or with spectral ranges removed
-4D sparse or with spectral ranges removed
-
-Combinations
-3D sparse with pixels missing
-4D sparse with pixels missing
-
-Collections
-1D collection offset (number of spectra that are not spectrally aligned)
-1D collection aligned (number of spectra that are spectrally aligned)
-
-Groups of collections
-1D collection offset (number of spectra that are not spectrally aligned)
-1D collection aligned (number of spectra that are spectrally aligned)
-For example, spectra from treated and untreated samples. 
-
-Groups of collections
-Spatially aware groups of 1D collections aligned (spectra from distinct regions of a 3D image)
-Spatially aware groups of 1D collections aligned (spectra from distinct regions of a 4D image)
-For example, we want to be able to handle the pixels (spectra) from distinct regions of a 
-hyperspectral image and work on them separately, before combining them back and rendering them in 2/3/4D. 
-We may also wish to know how close these distinct regions are. 
-
+% ##Data types ##
+% 
+% Need to be able to handle the following data types: 
+% 
+% 1D (spectrum)
+% 2D (picture)
+% 3D (hyperspectral image)
+% 4D (hyperspectral 3D image)
+% 
+% Spatially modified
+% 2D with pixels missing
+% 3D with pixels missing
+% 4D with pixels missing
+% 
+% Spectrally modified
+% 1D sparse or with spectral ranges removed
+% 3D sparse or with spectral ranges removed
+% 4D sparse or with spectral ranges removed
+% 
+% Combinations
+% 3D sparse with pixels missing
+% 4D sparse with pixels missing
+% 
+% Collections
+% 1D collection offset (number of spectra that are not spectrally aligned)
+% 1D collection aligned (number of spectra that are spectrally aligned)
+% 
+% Groups of collections
+% 1D collection offset (number of spectra that are not spectrally aligned)
+% 1D collection aligned (number of spectra that are spectrally aligned)
+% For example, spectra from treated and untreated samples. 
+% 
+% Groups of collections
+% Spatially aware groups of 1D collections aligned (spectra from distinct regions of a 3D image)
+% Spatially aware groups of 1D collections aligned (spectra from distinct regions of a 4D image)
+% For example, we want to be able to handle the pixels (spectra) from distinct regions of a 
+% hyperspectral image and work on them separately, before combining them back and rendering them in 2/3/4D. 
+% We may also wish to know how close these distinct regions are. 
 

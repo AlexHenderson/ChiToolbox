25170fcc93a1ebf9f7e0924e44bad39a445d2b70
Alex Henderson <alex.henderson@manchester.ac.uk>
2018-04-17 12:37:38 +0100

    Added example code and data including overfitting.

---
+++ Data/pccva_example.m
@@ -0,0 +1,29 @@
+fromfile = load('../Data/utidata.mat');
+data = ChiIRSpectralCollection(fromfile.wavenumbers,fromfile.uti);
+
+metadata = ChiMetadataSheet('../Data/uti_metadata.xlsx');
+
+data.classmembership = metadata.membership('Bacteria');
+data.plot('grouped','mean')
+
+data.vectornorm.removeco.plot('grouped','mean')
+
+pcaresult = data.pca();
+pcaresult.plotscores(1,2);
+
+pcaresult = data.vectornorm.pca();
+pcaresult.plotscores(1,2);
+pcaresult.plotloadings(1);
+pcaresult.plotloadings(2);
+
+cvaresult = data.vectornorm.pccva();
+cvaresult.plotscores(1,2);
+cvaresult.plotloadings(1);
+cvaresult.plotloadings(2);
+
+% overfitting
+cvaresult = data.vectornorm.pccva(30);
+cvaresult.plotscores(1,2);
+cvaresult.plotloadings(1);
+cvaresult.plotloadings(2);
+

00a65ed2fbbb98fb503099fcc524635ef7812a97
Alex Henderson <alex.henderson@manchester.ac.uk>
2018-04-22 20:59:18 +0100

    Moved to new cloning idiom.

--- CHIToolbox/@ChiAbstractImage/nthroot.m
+++ CHIToolbox/@ChiAbstractImage/nthroot.m
@@ -1,4 +1,4 @@
-function obj = nthroot(this,level)
+function obj = nthroot(varargin)
 
 % nthroot  Reduces the data to the given root
 %
@@ -24,19 +24,22 @@ function obj = nthroot(this,level)
 % If you use this file in your work, please acknowledge the author(s) in
 % your publications. 
 
-% Version 1.0, April 2018
 % The latest version of this file is available on Bitbucket
 % https://bitbucket.org/AlexHenderson/chitoolbox
 
 
+this = varargin{1};
+
 if nargout
     obj = this.clone();
-    % Not a great approach, but quite generic. Prevents errors if the
-    % function name changes
-    command = ['obj.', mfilename, '(level);'];
+    % Not a great approach, but quite generic. 
+    % Prevents errors if the function name changes. 
+    command = [mfilename, '(varargin{:});'];
     eval(command);  
 else
-    % We are expecting to modified this object in situ
+    % We are expecting to modify this object in situ
+    
+    level = varargin{2};
     this.data = nthroot(this.data,level);
     message = ['Reduce to root ', num2str(level)];
     this.history.add(message);

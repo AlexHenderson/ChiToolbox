4a0a96cc28f75b566ba6d222e58cbf3b9749432b
Alex Henderson <alex.henderson@manchester.ac.uk>
2018-05-25 17:49:32 +0100

    Added help documentation.

--- CHIToolbox/@ChiSpectralCollection/balance.m
+++ CHIToolbox/@ChiSpectralCollection/balance.m
@@ -1,7 +1,44 @@
 function [obj] = balance(this,varargin)
 
-% balance classes
-% default is to undersample
+% balance  Equalise class membership. 
+%
+% Syntax
+%   balance();
+%   balance(method);
+%   balanced = balance(____);
+%
+% Description
+%   balance() considers the number of members of each class in the
+%   collection and changes it to make these numbers equal. The default
+%   method is 'undersample'. This version modifies the original object.
+%
+%   balance(method) uses the approach specified by method to balance the
+%   classes. This version modifies the original object.
+%
+%   balanced = balance(____) first creates a clone of the object, then
+%   performs one of the balance functions on the clone. The original object
+%   is not modified.
+% 
+%   Only one balancing method is currently provided: undersample. See the
+%   documentation on undersample for that approach. 
+%
+% Copyright (c) 2018, Alex Henderson.
+% Licenced under the GNU General Public License (GPL) version 3.
+%
+% See also 
+%   utilities.undersample ChiSpectralCollection.
+
+% Contact email: alex.henderson@manchester.ac.uk
+% Licenced under the GNU General Public License (GPL) version 3
+% http://www.gnu.org/copyleft/gpl.html
+% Other licensing options are available, please contact Alex for details
+% If you use this file in your work, please acknowledge the author(s) in
+% your publications. 
+
+% Version 1.0, May 2018
+% The latest version of this file is available on Bitbucket
+% https://bitbucket.org/AlexHenderson/chitoolbox
+
 
 if isempty(this.classmembership)
     err = MException(['CHI:',mfilename,':IOError'], ...

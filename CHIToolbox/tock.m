69a1229118140578d29e96ff52b2de1bbe269ad4
Alex Henderson <alex.henderson@manchester.ac.uk>
2018-06-08 17:17:04 +0100

    Added additional output.

--- CHIToolbox/tock.m
+++ CHIToolbox/tock.m
@@ -3,18 +3,20 @@ function varargout = tock(tstart)
 % tock  An augmented version of toc. 
 %
 % Syntax
-%   elapsed = tock();
-%   elapsed = tock(tstart);
+%   [elapsed,seconds] = tock();
+%   [elapsed,seconds] = tock(tstart);
 %
 % Description
-%   elapsed = tock() is essentially the same as the built-in toc function,
+%   [elapsed,seconds] = tock() is essentially the same as the built-in toc function,
 %   reporting the time elapsed since the most recent call to tic. The
-%   output is formatted into weeks, days, hours, minutes and seconds.
+%   output elapsed is formatted into weeks, days, hours, minutes and
+%   seconds. seconds is the number of seconds since the previous tic
+%   command, equivalent of the toc output.
 %
-%   elapsed = tock(tstart) reports the time elapsed since the specific tic
-%   command that generated tstart.
+%   [elapsed,seconds] = tock(tstart) reports the time elapsed since the
+%   specific tic command that generated tstart.
 %
-% Copyright (c) 2017, Alex Henderson.
+% Copyright (c) 2017-2018, Alex Henderson.
 % Licenced under the GNU Lesser General Public License (LGPL) version 3.
 %
 % See also 
@@ -27,16 +29,26 @@ function varargout = tock(tstart)
 % If you use this file in your work, please acknowledge the author(s) in
 % your publications. 
 
-% Version 1.0, November 2017
+% Version 2.0, June 2018
 
 if exist('tstart','var')
-    elapsed = durationString(toc(tstart));
+    toc_output = toc(tstart);
 else
-    elapsed = durationString(toc());
+    toc_output = toc();
 end
 
-if (nargout > 0)
-    varargout{1} = elapsed;
-else
-    disp(['Elapsed time is ', elapsed, '.']);
+elapsed = durationString(toc_output);
+
+switch nargout
+    case 0
+        disp(['Elapsed time: ', elapsed, '.']);
+    case 1
+        varargout{1} = elapsed;
+    case 2
+        varargout{1} = elapsed;
+        varargout{2} = toc_output;
+    otherwise
+        varargout{1} = elapsed;
+        varargout{2} = toc_output;
+        disp(['Elapsed time: ', elapsed, '.']);
 end

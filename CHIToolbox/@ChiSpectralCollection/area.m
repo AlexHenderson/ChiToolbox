e7839a961162779e84c45ff269041e97e7c64d91
Alex Henderson <alex.henderson@manchester.ac.uk>
2018-08-14 12:34:24 +0100

    Added area under the spectrum function. Uses a linear baseline.

---
+++ CHIToolbox/@ChiSpectralCollection/area.m
@@ -0,0 +1,34 @@
+function output = area(this,lowx,highx)
+
+% area  The area under a linear baseline
+%
+% Syntax
+%   areavalues = area(lowx, highx);
+%
+% Description
+%   areavalues = area(lowx, highx) calculates the area under each spectrum
+%   between the limits provided in lowx and highx in x-axis units. A linear
+%   baseline is removed.
+%
+% Copyright (c) 2017-2018, Alex Henderson.
+% Licenced under the GNU General Public License (GPL) version 3.
+%
+% See also 
+%   ChiSpectralCollection.areaidx
+
+% Contact email: alex.henderson@manchester.ac.uk
+% Licenced under the GNU General Public License (GPL) version 3
+% http://www.gnu.org/copyleft/gpl.html
+% Other licensing options are available, please contact Alex for details
+% If you use this file in your work, please acknowledge the author(s) in
+% your publications. 
+
+% The latest version of this file is available on Bitbucket
+% https://bitbucket.org/AlexHenderson/chitoolbox
+
+    lowidx = indexat(this, lowx);
+    highidx = indexat(this, highx);
+
+    this.history.add(['measured area from ', num2str(lowx), ' to ', num2str(highx), ' x-axis units']);
+    output = this.areaidx(lowidx,highidx);
+end
---
+++ CHIToolbox/@ChiSpectralCollection/areaidx.m
@@ -0,0 +1,38 @@
+function output = areaidx(this,lowidx,highidx)
+
+% areaidx  The area under a linear baseline
+%
+% Syntax
+%   areavalues = areaidx(lowidx, highidx);
+%
+% Description
+%   areavalues = areaidx(lowidx, highidx) calculates the area under each
+%   spectrum between the index limits provided in lowidx and highidx. A
+%   linear baseline is removed.
+%
+% Copyright (c) 2017-2018, Alex Henderson.
+% Licenced under the GNU General Public License (GPL) version 3.
+%
+% See also 
+%   ChiSpectralCollection.area
+
+% Contact email: alex.henderson@manchester.ac.uk
+% Licenced under the GNU General Public License (GPL) version 3
+% http://www.gnu.org/copyleft/gpl.html
+% Other licensing options are available, please contact Alex for details
+% If you use this file in your work, please acknowledge the author(s) in
+% your publications. 
+
+% The latest version of this file is available on Bitbucket
+% https://bitbucket.org/AlexHenderson/chitoolbox
+
+    % Swap if 'from' is higher than 'to'
+    [lowidx,highidx] = ChiForceIncreasing(lowidx,highidx);
+
+    minvalue = min(this.data(:,lowidx:highidx),[],2);
+    shifteddata = this.data - repmat(minvalue,1,size(this.data, 2));
+    area = sum(shifteddata(:,lowidx:highidx),2);
+    triangle = (abs(shifteddata(:,highidx) - shifteddata(:,lowidx)) * (highidx-lowidx + 1)) ./ 2;
+    output = area - triangle;
+    this.history.add(['measured area from ', num2str(lowidx), ' to ', num2str(highidx), ' index postions']);
+end

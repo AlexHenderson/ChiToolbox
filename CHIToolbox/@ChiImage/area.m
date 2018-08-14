ffcf78fe8dc917e3d07d9381b959f16d2e29289f
Alex Henderson <alex.henderson@manchester.ac.uk>
2018-08-14 12:33:56 +0100

    Added help info.

--- CHIToolbox/@ChiImage/area.m
+++ CHIToolbox/@ChiImage/area.m
@@ -1,10 +1,36 @@
 function output = area(this,lowx,highx)
-% Generate ChiPicture of a proportion of spectral ranges using x values
-% Copyright (c) 2014 Alex Henderson (alex.henderson@manchester.ac.uk)
+
+% area  The area under a linear baseline
+%
+% Syntax
+%   areapicture = area(lowx, highx);
+%
+% Description
+%   areapicture = area(lowx, highx) calculates the area under the spectrum
+%   of each pixel between the limits provided in lowx and highx in x-axis
+%   units. A linear baseline is removed. A ChiPicture is produced in
+%   areapicture.
+%
+% Copyright (c) 2017-2018, Alex Henderson.
+% Licenced under the GNU General Public License (GPL) version 3.
+%
+% See also 
+%   ChiImage.areaidx ChiPicture.
+
+% Contact email: alex.henderson@manchester.ac.uk
+% Licenced under the GNU General Public License (GPL) version 3
+% http://www.gnu.org/copyleft/gpl.html
+% Other licensing options are available, please contact Alex for details
+% If you use this file in your work, please acknowledge the author(s) in
+% your publications. 
+
+% The latest version of this file is available on Bitbucket
+% https://bitbucket.org/AlexHenderson/chitoolbox
 
     lowidx = indexat(this, lowx);
     highidx = indexat(this, highx);
 
-    this.history.add(['measurearea from ', num2str(lowx), ' to ', num2str(highx)]);
+    this.history.add(['measured area from ', num2str(lowx), ' to ', num2str(highx), ' x-axis units']);
     output = this.areaidx(lowidx,highidx);
+    output.history.add(['measured area from ', num2str(lowx), ' to ', num2str(highx), ' x-axis units']);
 end
--- CHIToolbox/@ChiImage/areaidx.m
+++ CHIToolbox/@ChiImage/areaidx.m
@@ -1,6 +1,31 @@
 function output = areaidx(this,lowidx,highidx)
-% Generate ChiPicture of a proportion of spectral ranges using index values
-% Copyright (c) 2014 Alex Henderson (alex.henderson@manchester.ac.uk)
+
+% areaidx  The area under a linear baseline
+%
+% Syntax
+%   areapicture = areaidx(lowidx, highidx);
+%
+% Description
+%   areapicture = areaidx(lowidx, highidx) calculates the area under the
+%   spectrum of each pixel between the index limits provided in lowidx and
+%   highidx. A linear baseline is removed. A ChiPicture is produced in
+%   areapicture.
+%
+% Copyright (c) 2017-2018, Alex Henderson.
+% Licenced under the GNU General Public License (GPL) version 3.
+%
+% See also 
+%   ChiImage.area ChiPicture.
+
+% Contact email: alex.henderson@manchester.ac.uk
+% Licenced under the GNU General Public License (GPL) version 3
+% http://www.gnu.org/copyleft/gpl.html
+% Other licensing options are available, please contact Alex for details
+% If you use this file in your work, please acknowledge the author(s) in
+% your publications. 
+
+% The latest version of this file is available on Bitbucket
+% https://bitbucket.org/AlexHenderson/chitoolbox
 
     % Swap if 'from' is higher than 'to'
     [lowidx,highidx] = ChiForceIncreasing(lowidx,highidx);
@@ -11,6 +36,6 @@ function output = areaidx(this,lowidx,highidx)
     triangle = (abs(shifteddata(:,highidx) - shifteddata(:,lowidx)) * (highidx-lowidx + 1)) ./ 2;
     area = area - triangle;
     output = ChiPicture(area,this.xpixels,this.ypixels);
-    output.history.add(['areaindex: from ', num2str(lowidx), ' to ', num2str(highidx)]);
-    this.history.add(['areaindex: from ', num2str(lowidx), ' to ', num2str(highidx)]);
+    output.history.add(['measured area: from ', num2str(lowidx), ' to ', num2str(highidx), ' index postions']);
+    this.history.add(['measured area: from ', num2str(lowidx), ' to ', num2str(highidx), ' index postions']);
 end

3a43a670035c488f8422063ae9a427417c0fb9f8
Alex Henderson <alex.henderson@manchester.ac.uk>
2018-08-24 11:39:38 +0100

    Updated ontology terms.

--- CHIToolbox/@ChiToFMassSpecImage/ChiToFMassSpecImage.m
+++ CHIToolbox/@ChiToFMassSpecImage/ChiToFMassSpecImage.m
@@ -40,15 +40,15 @@ classdef ChiToFMassSpecImage < ChiMassSpecImage & ChiToFMSCharacter
 % https://bitbucket.org/AlexHenderson/chitoolbox
 
 
-%     % As close as we can get
-%     properties (Constant)
-%         % ontology_term time-of-flight mass spectrum;
-%         ontology_term = 'time-of-flight mass spectrum';
-%         % type A plot of relative abundance (%) vs. mass-to-charge ratio obtained from a mass spectrometry experiment where the mass-to-charge ratio is determined from the time they take to reach a detector.
-%         type = 'A plot of relative abundance (%) vs. mass-to-charge ratio obtained from a mass spectrometry experiment where the mass-to-charge ratio is determined from the time they take to reach a detector.';
-%         % uri http://purl.obolibrary.org/obo/CHMO_0000828
-%         uri = 'http://purl.obolibrary.org/obo/CHMO_0000828'
-%     end    
+    % As close as we can get
+    properties  % Can't be Constant since we inherit from MassSpectralCharacter
+        % ontology_term time-of-flight mass spectrum;
+        ontology_term = 'time-of-flight mass spectrum';
+        % type A plot of relative abundance (%) vs. mass-to-charge ratio obtained from a mass spectrometry experiment where the mass-to-charge ratio is determined from the time they take to reach a detector.
+        type = 'A plot of relative abundance (%) vs. mass-to-charge ratio obtained from a mass spectrometry experiment where the mass-to-charge ratio is determined from the time they take to reach a detector.';
+        % uri http://purl.obolibrary.org/obo/CHMO_0000828
+        uri = 'http://purl.obolibrary.org/obo/CHMO_0000828'
+    end    
     
     methods
         

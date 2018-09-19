24891bae5b0a37ef1d4aba0b0af51b954b7b4302
Alex Henderson <alex.henderson@manchester.ac.uk>
2018-09-19 16:55:07 +0100

    Added functions to draw zero axis lines.

--- CHIToolbox/+utilities/drawx0axis.m
+++ CHIToolbox/+utilities/drawx0axis.m
@@ -1,19 +1,19 @@
-function draw00axes(axis)
+function drawx0axis(axis)
 
-% draw00axes  Draw lines indicating x=0 and y=0 on a chart
+% drawx0axis  Draw line indicating x=0 on a chart
 %
 % Syntax
-%   draw00axes(axis);
+%   drawx0axis(axis);
 %
 % Description
-%   draw00axes(axis) takes the axis limits of a chart and plots x=0 and y=0
-%   on that chart
+%   drawx0axis(axis) takes the axis limits of a chart and plots x=0 on that
+%   chart
 % 
 % Copyright (c) 2018, Alex Henderson.
 % Licenced under the GNU General Public License (GPL) version 3.
 %
 % See also 
-%   drawx0axis, drawy0axis, plot scatter, gscatter.
+%   drawy0axis, draw00axes, plot scatter, gscatter.
 
 % Contact email: alex.henderson@manchester.ac.uk
 % Licenced under the GNU General Public License (GPL) version 3
@@ -30,16 +30,8 @@ axiscolour = 'k';
 
 hold on;
 limits = axis;
-xmin = limits(1,1);
-xmax = limits(1,2);
 ymax = limits(1,3);
 ymin = limits(1,4);
-h = plot([0,0], [0,ymax], axiscolour);
-set(get(get(h,'Annotation'),'LegendInformation'),'IconDisplayStyle','off');
-h = plot([0,0], [0,ymin], axiscolour);
-set(get(get(h,'Annotation'),'LegendInformation'),'IconDisplayStyle','off');
-h = plot([0,xmax], [0,0], axiscolour);
-set(get(get(h,'Annotation'),'LegendInformation'),'IconDisplayStyle','off');
-h = plot([0,xmin], [0,0], axiscolour);
+h = plot([0,0],[ymin,ymax], axiscolour);
 set(get(get(h,'Annotation'),'LegendInformation'),'IconDisplayStyle','off');
 hold off;    
--- CHIToolbox/+utilities/drawy0axis.m
+++ CHIToolbox/+utilities/drawy0axis.m
@@ -1,19 +1,19 @@
-function draw00axes(axis)
+function drawy0axis(axis)
 
-% draw00axes  Draw lines indicating x=0 and y=0 on a chart
+% drawy0axis  Draw line indicating y=0 on a chart
 %
 % Syntax
-%   draw00axes(axis);
+%   drawy0axis(axis);
 %
 % Description
-%   draw00axes(axis) takes the axis limits of a chart and plots x=0 and y=0
-%   on that chart
+%   drawy0axis(axis) takes the axis limits of a chart and plots y=0 on that
+%   chart
 % 
 % Copyright (c) 2018, Alex Henderson.
 % Licenced under the GNU General Public License (GPL) version 3.
 %
 % See also 
-%   drawx0axis, drawy0axis, plot scatter, gscatter.
+%   drawx0axis, draw00axes, plot scatter, gscatter.
 
 % Contact email: alex.henderson@manchester.ac.uk
 % Licenced under the GNU General Public License (GPL) version 3
@@ -32,14 +32,6 @@ hold on;
 limits = axis;
 xmin = limits(1,1);
 xmax = limits(1,2);
-ymax = limits(1,3);
-ymin = limits(1,4);
-h = plot([0,0], [0,ymax], axiscolour);
-set(get(get(h,'Annotation'),'LegendInformation'),'IconDisplayStyle','off');
-h = plot([0,0], [0,ymin], axiscolour);
-set(get(get(h,'Annotation'),'LegendInformation'),'IconDisplayStyle','off');
-h = plot([0,xmax], [0,0], axiscolour);
-set(get(get(h,'Annotation'),'LegendInformation'),'IconDisplayStyle','off');
-h = plot([0,xmin], [0,0], axiscolour);
+h = plot([xmin,xmax], [0,0], axiscolour);
 set(get(get(h,'Annotation'),'LegendInformation'),'IconDisplayStyle','off');
 hold off;    

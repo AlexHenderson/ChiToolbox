ba72e56eb6220c9fc70eb919bd284178de4221c5
Alex Henderson <alex.henderson@manchester.ac.uk>
2019-01-21 10:53:28 +0000

    Modified to accept user defined options and added help information. Normalisation now the default. Inversion is an option.

--- CHIToolbox/@ChiImagePCAOutcome/imagepc.m
+++ CHIToolbox/@ChiImagePCAOutcome/imagepc.m
@@ -1,54 +1,112 @@
-function imagepc(this, pc, varargin)
+function handle = imagepc(this,pc,varargin)
+
+% imagepc  Creates an image of the score on the selected principal component.
+%
+% Syntax
+%   imagepc(this,pc);
+%   imagepc(____, 'nonorm');
+%   imagepc(____, 'invert');
+%   imagepc(____, 'nofig');
+%   handle = imagepc(____);
+%
+% Description
+%   imagepc(this,pc) creates an image of the score on the selected
+%   principal component. The positive and negative going scores are each
+%   normalised to unity.
+% 
+%   imagepc(____, 'nonorm') creates an image of the score without
+%   normalisation.
+%
+%   imagepc(____, 'invert') multiplies the data by -1 to invert the
+%   direction of the scores. This can be useful when comparing different
+%   packages. Do not forget to invert the loading of the specified
+%   principal component also. 
+%
+%   handle = imagepc(____) returns a handle to this figure.
+% 
+% Copyright (c) 2018-2019, Alex Henderson.
+% Licenced under the GNU General Public License (GPL) version 3.
+%
+% See also 
+%   ChiImagePCAOutcome ChiPicture  imagesc
+
+% Contact email: alex.henderson@manchester.ac.uk
+% Licenced under the GNU General Public License (GPL) version 3
+% http://www.gnu.org/copyleft/gpl.html
+% Other licensing options are available, please contact Alex for details
+% If you use this file in your work, please acknowledge the author(s) in
+% your publications. 
+
+% The latest version of this file is available on Bitbucket
+% https://bitbucket.org/AlexHenderson/chitoolbox
+
 
 % ToDo: handle masked data
 
-%% Do we need a new figure?
-argposition = find(cellfun(@(x) strcmpi(x, 'nofig') , varargin));
-if argposition
-    % Remove the parameter from the argument list
-    varargin(argposition) = [];
-else
-    % No 'nofig' found so create a new figure
-    figure;
-end
+    % Get user defined options
+    invert = false;
+    normalise = true;
 
+    argposition = find(cellfun(@(x) strcmpi(x, 'invert') , varargin));
+    if argposition
+        varargin(argposition) = [];
+        invert = ~invert;
+    end
+
+    argposition = find(cellfun(@(x) strcmpi(x, 'nonorm') , varargin));
+    if argposition
+        varargin(argposition) = [];
+        normalise = ~normalise;
+    end
 
     if ~isempty(this.loadings)
+        % Do we need a new figure
+        argposition = find(cellfun(@(x) strcmpi(x, 'nofig') , varargin));
+        if argposition
+            varargin(argposition) = [];
+        else
+            if nargout
+                handle = figure;
+            else
+                figure;
+            end
+        end
+        
         if (pc > this.numpcs)
             err = MException('CHI:ChiImagePCAOutcome:OutOfRange', ...
                 ['Requested principal component is out of range. Max PCs = ', num2str(this.numpcs), '.']);
             throw(err);
         end
 
-        invert = false;
-        normalise = true;
-        % ToDo: fix masked data
-        toplot = reshape(this.scores,this.height,this.width,this.numpcs);
-        pc_to_plot = toplot(:,:,pc);
+        scores3d = reshape(this.scores,this.height,this.width,this.numpcs);
+        pc_to_plot = scores3d(:,:,pc);
 
-        % window_title = ['PC ', int2str(pc)];
+        window_title = ['Score on principal component ', int2str(pc)];
         figure_title = ['Score on principal component ', int2str(pc)];
 
         if invert
             pc_to_plot = pc_to_plot * -1;
-        %     window_title = [window_title, ' (inverted)'];
+            window_title = [window_title, ' (inverted)'];
             figure_title = [figure_title, ' (inverted)'];
         end
-        % handle = figure('Name',window_title,'NumberTitle','off');
+
+        pic = ChiPicture(pc_to_plot);
 
         if normalise
-            pc_to_plot = this.normscores(pc_to_plot);
+            pic.normbimodal();
         end
 
-        imagesc(pc_to_plot,varargin{:});
-        axis image;
+        pic.imagesc(varargin{:});
 
+        fig = gcf;
+        fig.Name = window_title;
+        fig.NumberTitle = 'off';
+        
         title(figure_title);
         set(gca,'XTickLabel','');
         set(gca,'YTickLabel','');
         colorbar;        
         
-        colormap(ChiBimodalColormap());
     end
 
 end

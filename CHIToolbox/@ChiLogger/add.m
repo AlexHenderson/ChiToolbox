34c7ceee9324ba4493c684f3df5c282262df85f9
Alex Henderson <alex.henderson@manchester.ac.uk>
2019-02-14 15:53:15 +0000

    Added ability to transparently append different types of log history,

--- CHIToolbox/@ChiLogger/add.m
+++ CHIToolbox/@ChiLogger/add.m
@@ -1,11 +1,26 @@
 function add(this,content)
-% add Adds an entry to the log
+
+% add  Adds an entry to the log
 % Copyright (c) 2014 Alex Henderson (alex.henderson@manchester.ac.uk)
 
-    if isempty(this.log{1})
-        this.log{1} = content;
-    else
-        this.log = vertcat(this.log,content);
-    end
 
+if exist('content','var')
+    
+    if isa(content,'ChiLogger')
+        this.log = vertcat(this.log,content.log);
+    else
+        if iscellstr(content) %#ok<ISCLSTR>
+            this.log = vertcat(this.log,content);
+        else
+            if ischar(content)
+                this.log = vertcat(this.log,{content});
+            else
+                err = MException(['CHI:',mfilename,':TypeError'], ...
+                    'Unable to append this history log.');
+                throw(err);
+            end
+        end
+    end
 end
+
+end % function: add

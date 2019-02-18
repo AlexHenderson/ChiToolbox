d605cbb8d6a38ecc4f946b3952f65187a3242050
Alex Henderson <alex.henderson@manchester.ac.uk>
2019-02-18 10:58:08 +0000

    Added help information.

--- CHIToolbox/@ChiIRCharacter/abs2trans.m
+++ CHIToolbox/@ChiIRCharacter/abs2trans.m
@@ -1,4 +1,34 @@
 function obj = abs2trans(this)
+    
+% trans2abs  Convert data from absorbance units to transmittance units
+%
+% Syntax
+%   abs2trans();
+%   obj = abs2trans();
+%
+% Description
+%   abs2trans() changes the data from absorbance units to transmittance
+%   units.
+% 
+%   obj = abs2trans() clones the object before changing the unit. 
+%
+% Copyright (c) 2018, Alex Henderson.
+% Licenced under the GNU General Public License (GPL) version 3.
+%
+% See also 
+%   trans2abs ChiIRCharacter.
+
+% Contact email: alex.henderson@manchester.ac.uk
+% Licenced under the GNU General Public License (GPL) version 3
+% http://www.gnu.org/copyleft/gpl.html
+% Other licensing options are available, please contact Alex for details
+% If you use this file in your work, please acknowledge the author(s) in
+% your publications. 
+
+% The latest version of this file is available on Bitbucket
+% https://bitbucket.org/AlexHenderson/chitoolbox
+
+
     if nargout
         % Want to send output to a new object so clone and re-enter
         % the function from the clone
@@ -12,4 +42,5 @@ function obj = abs2trans(this)
         this.ylabel = 'transmittance (%)'; 
         this.history.add('Converted to percentage transmittance');
     end
-end
+    
+end % function abs2trans
--- CHIToolbox/@ChiIRCharacter/trans2abs.m
+++ CHIToolbox/@ChiIRCharacter/trans2abs.m
@@ -1,4 +1,34 @@
 function obj = trans2abs(this)
+
+% trans2abs  Convert data from transmittance units to absobance units
+%
+% Syntax
+%   trans2abs();
+%   obj = trans2abs();
+%
+% Description
+%   trans2abs() changes the data from transmittance units to absorbance
+%   units.
+% 
+%   obj = trans2abs() clones the object before changing the unit. 
+%
+% Copyright (c) 2018, Alex Henderson.
+% Licenced under the GNU General Public License (GPL) version 3.
+%
+% See also 
+%   abs2trans ChiIRCharacter.
+
+% Contact email: alex.henderson@manchester.ac.uk
+% Licenced under the GNU General Public License (GPL) version 3
+% http://www.gnu.org/copyleft/gpl.html
+% Other licensing options are available, please contact Alex for details
+% If you use this file in your work, please acknowledge the author(s) in
+% your publications. 
+
+% The latest version of this file is available on Bitbucket
+% https://bitbucket.org/AlexHenderson/chitoolbox
+
+
     if nargout
         % Want to send output to a new object so clone and re-enter
         % the function from the clone

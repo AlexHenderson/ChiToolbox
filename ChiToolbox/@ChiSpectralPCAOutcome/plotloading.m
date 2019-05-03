48610757f6c6998dc70ed38d7459070835d9a8d2
Alex Henderson <alex.henderson@manchester.ac.uk>
2019-05-03 18:54:04 +0100

    Improved error handling.

--- ChiToolbox/@ChiSpectralPCAOutcome/plotloading.m
+++ ChiToolbox/@ChiSpectralPCAOutcome/plotloading.m
@@ -19,7 +19,7 @@ function plotloading(this,pc,varargin)
 %   Other parameters can be applied to customise the plot. See the MATLAB
 %   plot/bar functions for more details. 
 %
-% Copyright (c) 2017-2018, Alex Henderson.
+% Copyright (c) 2017-2019, Alex Henderson.
 % Licenced under the GNU General Public License (GPL) version 3.
 %
 % See also 
@@ -38,67 +38,65 @@ function plotloading(this,pc,varargin)
 % https://bitbucket.org/AlexHenderson/chitoolbox
 
 
-    titlestub = 'Loading on principal component ';
-    windowtitlestub = titlestub;
-    ylabelstub = 'loading on PC ';
-    errorcode = 'CHI:ChiSpectralPCAOutcome';
-    errormessagestub = 'Requested principal component is out of range. Max PCs = ';
-    
-    barplot = false;
+%% Error checking
+if isempty(this.loadings)
+    err = MException(['CHI:',mfilename,':InputError'], ...
+        'No loadings are available.');
+    throw(err);
+end
 
-    if ~isempty(this.loadings)
-        if ((pc > this.numpcs) || (pc < 1))
-            err = MException([errorcode,':OutOfRange'], ...
-                [errormessagestub, num2str(this.numpcs), '.']);
-            throw(err);
-        end
+if ((pc > this.numpcs) || (pc < 1))
+    err = MException(['CHI:',mfilename,':InputError'], ...
+        ['Requested principal component is out of range. Max PCs = ',utilities.tostring(this.numpcs)]);
+    throw(err);
+end
 
-        argposition = find(cellfun(@(x) strcmpi(x, 'nofig') , varargin));
-        if argposition
-            % Remove the parameter from the argument list
-            varargin(argposition) = [];
-        else
-            % No 'nofig' found so create a new figure
-            windowtitle = [windowtitlestub, num2str(pc)];
-            figure('Name',windowtitle,'NumberTitle','off');
-        end
+titlestub = 'Loading on principal component ';
+windowtitlestub = titlestub;
+ylabelstub = 'loading on PC ';
 
-        argposition = find(cellfun(@(x) strcmpi(x, 'bar') , varargin));
-        if argposition
-            % Remove the parameter from the argument list
-            varargin(argposition) = [];
-            barplot = true;
-        end
-        
-        datatoplot = this.loadings(:,pc);
-        if barplot
-            bar(this.xvals, datatoplot, varargin{:});
-        else
-            plot(this.xvals, datatoplot, varargin{:});
-        end
-        
-        if this.reversex
-            set(gca,'XDir','reverse');
-        end
-        
-        utilities.tightxaxis;
-        
-        if ~barplot
-            utilities.drawy0axis(axis);
-        end
-        
-        xlabel(this.xlabel);        
-        ylabel([ylabelstub, num2str(pc), ' (', num2str(this.explained(pc),3), '%)']);
-        title([titlestub, num2str(pc)]);
-        
-        % Manage data cursor information
-        figurehandle = gcf;
-        cursor = datacursormode(figurehandle);
-        set(cursor,'UpdateFcn',@utilities.datacursor_6sf);
+barplot = false;
 
-        
-    end
-    
+%% Parse command line
+argposition = find(cellfun(@(x) strcmpi(x, 'nofig') , varargin));
+if argposition
+    % Remove the parameter from the argument list
+    varargin(argposition) = [];
+else
+    % No 'nofig' found so create a new figure
+    windowtitle = [windowtitlestub, num2str(pc)];
+    figure('Name',windowtitle,'NumberTitle','off');
+end
+
+argposition = find(cellfun(@(x) strcmpi(x, 'bar') , varargin));
+if argposition
+    % Remove the parameter from the argument list
+    varargin(argposition) = [];
+    barplot = true;
+end
+
+%% Generate plot
+datatoplot = this.loadings(:,pc);
+if barplot
+    bar(this.xvals, datatoplot, varargin{:});
+else
+    plot(this.xvals, datatoplot, varargin{:});
+end
+
+if this.reversex
+    set(gca,'XDir','reverse');
+end
+
+utilities.tightxaxis;
+
+if ~barplot
+    utilities.drawy0axis(axis);
+end
+
+xlabel(this.xlabel);        
+ylabel([ylabelstub, num2str(pc), ' (', num2str(this.explained(pc),3), '%)']);
+title([titlestub, num2str(pc)]);
+
 %% Manage data cursor information
 figurehandle = gcf;
 cursor = datacursormode(figurehandle);

313cc00d330e0e7d2a0213aba34068adc3413a07
Alex Henderson <alex.henderson@manchester.ac.uk>
2019-05-14 12:03:51 +0100

    Renamed removeco to removeco2.

---
+++ ChiToolbox/@ChiRamanCharacter/removeco2.m
@@ -0,0 +1,46 @@
+function obj = removeco2(varargin)
+
+% removeco2  Removes the CO2 region from the spectrum (2250-2450 wavenumbers). 
+%
+% Syntax
+%   removeco2();
+%   modified = removeco2();
+%
+% Description
+%   removeco2() removes the CO2 region of the spectrum delimited 2250 and
+%   2450 wavenumbers. This version modifies the original object.
+%
+%   modified = removeco2() first creates a clone of the object, then
+%   removes the CO2 region of the spectrum from the clone. The original
+%   object is not modified.
+%
+% Copyright (c) 2017-2019, Alex Henderson.
+% Licenced under the GNU General Public License (GPL) version 3.
+%
+% See also 
+%   removerange ChiRamanSpectrum.
+
+% Contact email: alex.henderson@manchester.ac.uk
+% Licenced under the GNU General Public License (GPL) version 3
+% http://www.gnu.org/copyleft/gpl.html
+% Other licensing options are available, please contact Alex for details
+% If you use this file in your work, please acknowledge the author(s) in
+% your publications. 
+
+% The latest version of this file is available on Bitbucket
+% https://bitbucket.org/AlexHenderson/chitoolbox
+
+
+    this = varargin{1};
+
+    if nargout
+        obj = this.clone();
+        command = [mfilename, '(obj,varargin{2:end});'];
+        eval(command);  
+    else
+        this.removerange(2250,2450);
+        message = 'CO2 region removed';
+        this.history.add(message);
+    end
+
+end % function removeco2

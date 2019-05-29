41c20f8c5d0ab69b5f351c36c310f0e9f9e09a41
Alex Henderson <alex.henderson@manchester.ac.uk>
2019-05-29 16:44:30 +0100

    Added help information.

--- ChiToolbox/@ChiAbstractImage/sum.m
+++ ChiToolbox/@ChiAbstractImage/sum.m
@@ -1,11 +1,35 @@
-function spectrum = sum(this)
+function sumspectrum = sum(this)
 
-% SUM
+% sum  Calculates the sum of the spectra in the image. 
+%
+% Syntax
+%   sumspectrum = sum();
+%
+% Description
+%   sumspectrum = sum() calculates the sum of the spectra in the image.
+%   sumspectrum is the appropriate Chi*Spectrum
+%
+% Copyright (c) 2018, Alex Henderson.
+% Licenced under the GNU General Public License (GPL) version 3.
+%
+% See also 
+%   mean median.
+
+% Contact email: alex.henderson@manchester.ac.uk
+% Licenced under the GNU General Public License (GPL) version 3
+% http://www.gnu.org/copyleft/gpl.html
+% Other licensing options are available, please contact Alex for details
+% If you use this file in your work, please acknowledge the author(s) in
+% your publications. 
+
+% The latest version of this file is available on Bitbucket
+% https://bitbucket.org/AlexHenderson/chitoolbox
+
 
     spectrumclass = str2func(this.spectrumclassname);
-    spectrum = spectrumclass(this.xvals,ChiSum(this.data),this.reversex,...
+    sumspectrum = spectrumclass(this.xvals,ChiSum(this.data),this.reversex,...
         this.xlabelname,this.xlabelunit,this.ylabelname,this.ylabelunit,...
         this.classmembership,this.filenames,this.history);
-    spectrum.history.add('Sum spectrum of image');
+    sumspectrum.history.add('Sum spectrum of image');
 
 end

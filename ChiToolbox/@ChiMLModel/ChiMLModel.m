8ebb10d89f5858ab33fc52179f4da5ef139ca761
Alex Henderson <alex.henderson@manchester.ac.uk>
2019-10-08 17:53:45 +0100

    Updated help information.

--- ChiToolbox/@ChiMLModel/ChiMLModel.m
+++ ChiToolbox/@ChiMLModel/ChiMLModel.m
@@ -1,16 +1,43 @@
 classdef ChiMLModel < ChiBase
-% ChiMLModel
-%   Copyright (c) 2018-2019 Alex Henderson (alex.henderson@manchester.ac.uk)
+
+% ChiMLModel  Class to manage the outcome of machine learning algorithms.
+%
+% Syntax
+%   model = ChiMLModel(trainingmask,testmask,algorithm,compactmodel,
+%                       prediction,scores,stdevs,correctlyclassified,
+%                       classmembership,elapsed,elaspedinseconds,
+%                       predictiontime,predictionsec)
+%
+% Description
+%   model = ChiMLModel(____) creates a wrapper for a model produced by a
+%   machine learning algorithm, typically random forest or adaboost
+%
+% Copyright (c) 2018-2019, Alex Henderson.
+% Licenced under the GNU General Public License (GPL) version 3.
+%
+% See also 
+%   randomforest adaboost fitcensemble.
+
+% Contact email: alex.henderson@manchester.ac.uk
+% Licenced under the GNU General Public License (GPL) version 3
+% http://www.gnu.org/copyleft/gpl.html
+% Other licensing options are available, please contact Alex for details
+% If you use this file in your work, please acknowledge the author(s) in
+% your publications. 
+
+% The latest version of this file is available on Bitbucket
+% https://bitbucket.org/AlexHenderson/chitoolbox
+
 
     properties
-        trainmask;
-        testmask;
-        algorithm;
-        model;
-        prediction;
-        scores;
-        stdevs;
-        correctlyclassified;
+        trainmask;  % Logical column vector where true values indicate spectra to train the model
+        testmask;   % Logical column vector where true values indicate spectra to test the model
+        algorithm;  % String describing the algorithm used to generate the model
+        model;      % Compact model output from fitcensemble or TreeBagger
+        prediction; % Column vector of class label identifiers containing the prediction for each test spectrum
+        scores;     % Score of the prediction for each spectrum and each label
+        stdevs;     % Standard deviation of label predictions (TreeBagger only)
+        correctlyclassified;    % Column vector of logical values where true indicates a test spectrum correctly classified
         classmembership;        % An instance of ChiClassMembership
         elapsed;                % Time taken to generate the model (string)
         elaspedinseconds;       % Time taken to generate the model in seconds
@@ -20,12 +47,12 @@ classdef ChiMLModel < ChiBase
     end
     
     properties (Dependent = true)
-        %% Calculated properties
-        cc;
-        percentcorrectlyclassified;
-        percentcc;
-        pcc;
-        numtrees;
+        cc; % Column vector of logical values where true indicates a test spectrum correctly classified
+        percentcorrectlyclassified; % Percentage of test data correctly classified
+        percentcc;  % Percentage of test data correctly classified
+        pcc;    % Percentage of test data correctly classified
+        numtrees;   % Number of classifiers in the ensemble. Decision trees for random forest, or iterations for AdaBoost
+        iterations; % Number of classifiers in the ensemble. Decision trees for random forest, or iterations for AdaBoost
     end
     
     methods
@@ -100,6 +127,11 @@ classdef ChiMLModel < ChiBase
                     numtrees = this.model.NumTrained;
                  end
             end
+        end
+        
+        %% iterations
+        function iterations = get.iterations(this)
+            iterations = this.numtrees;
         end
         
     end

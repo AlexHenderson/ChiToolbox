05b8c5e33f8a24ef53661e5e8d157c98a6de4f5b
Alex Henderson <alex.henderson@manchester.ac.uk>
2019-11-08 15:23:45 +0000

    Updated peak detection to accept and output a peak list.

--- ChiToolbox/@ChiMSCharacter/peakdetect.m
+++ ChiToolbox/@ChiMSCharacter/peakdetect.m
@@ -5,15 +5,19 @@ function obj = peakdetect(varargin)
 % Syntax
 %   peakdetect();
 %   peakdetect(Name,Value);
+%   peakdetect(____,'vis');
 %   modified = peakdetect(____);
 %
 % Description
 %   peakdetect() detects peaks in the data and reduces the data to
 %   centroids using default values (see below). 
 % 
-%   peakdetect(Name,Value) uses Name/Value pairs to change the default
-%   values to those provided. 
+%   peakdetect(Name,Value) uses Name/Value pairs to replace the default
+%   values with those provided. 
 %
+%   peakdetect(____,'vis') display a window overlaying the data with the
+%   detected centroids
+% 
 %   modified = peakdetect(____) first creates a clone of the object, then
 %   detects peaks in the clone. The original object is not modified.
 %
@@ -22,10 +26,6 @@ function obj = peakdetect(varargin)
 %     gwindow = 30, the width the Gaussian kernel function;
 %     d2window = 21, the width of the second derivative window;
 %     numberlimit = 500, limits detected peaks to the Nth most intense;
-%     vis = false, whether to display a window overlaying the peaks with
-%                   the detected centroids;
-%     plotscaler = 20, a parameter to upscale the original spectrum in any
-%                       visualisation. Ignored if vis == false.
 %
 % Notes
 %   The peak detection process is as follows:
@@ -47,7 +47,7 @@ function obj = peakdetect(varargin)
 %        total ion spectrum become the new mass positions for each of these
 %        peaks. 
 %     
-% Copyright (c) 2018, Alex Henderson.
+% Copyright (c) 2018-2019, Alex Henderson.
 % Licenced under the GNU General Public License (GPL) version 3.
 %
 % See also 
@@ -60,7 +60,6 @@ function obj = peakdetect(varargin)
 % If you use this file in your work, please acknowledge the author(s) in
 % your publications. 
 
-% Version 1.0, September 2018
 % The latest version of this file is available on Bitbucket
 % https://bitbucket.org/AlexHenderson/chitoolbox
 
@@ -77,17 +76,24 @@ if nargout
     obj = this.clone();
     command = [mfilename, '(obj,varargin{2:end});'];
     eval(command);  
-else
-    
+else    
     %% Set default parameters
     gsigma = 5;
     gwindow = 30;
     d2window = 21;
     numberlimit = 500;
     vis = false;
-    plotscaler = 20;
+    peaktable = [];
+
+    %% Get parameters from the user
+    argposition = find(cellfun(@(x) strcmpi(x, 'peaktable') , varargin));
+    if argposition
+        % Remove the parameters from the argument list
+        peaktable = varargin{argposition+1};
+        varargin(argposition + 1) = [];
+        varargin(argposition) = [];
+    end
     
-    % Get parameters from the user
     argposition = find(cellfun(@(x) strcmpi(x, 'gsigma') , varargin));
     if argposition
         % Remove the parameters from the argument list
@@ -123,179 +129,132 @@ else
     argposition = find(cellfun(@(x) strcmpi(x, 'vis') , varargin));
     if argposition
         % Remove the parameters from the argument list
-        vis = varargin{argposition+1};
-        varargin(argposition + 1) = [];
+        vis = true;
         varargin(argposition) = [];
     end
     
-    argposition = find(cellfun(@(x) strcmpi(x, 'plotscaler') , varargin));
+    argposition = find(cellfun(@(x) strcmpi(x, 'viz') , varargin));
     if argposition
         % Remove the parameters from the argument list
-        plotscaler = varargin{argposition+1};
-        varargin(argposition + 1) = [];
+        vis = true;
         varargin(argposition) = [];
     end
     
-    if (length(varargin) > 1)
+    if (length(varargin) > 2)
         utilities.warningnobacktrace(['Parameter ', num2str(varargin{2}), ' not understood']);
     end
     
-    %% Identify some data to peak detect
-    if isa(this, 'ChiSpectrum')
-        spectrum = this.data;
-    else
-        spectrum = sum(this.data);
-    end
-    
-    % Generate a Gaussian smoothing kernel
-    % https://stackoverflow.com/questions/6992213/gaussian-filter-on-a-vector-in-matlab
-    x = linspace(-gwindow/2, gwindow/2, gwindow);
-    gaussFilter = exp(-x.^2 / (2 * gsigma^2));
-    gaussFilter = gaussFilter / sum(gaussFilter);   % normalize
-
-    % Smooth the spectrum using convolution
-    convoluted = conv(spectrum,gaussFilter,'same');
-
-    % Take second derivative of the smoothed spectrum
-    convoluted2nd = utilities.secondderiv(this.mass,convoluted,d2window);
-
-    % Remove any positive going features
-    convoluted2nd(convoluted2nd > 0) = 0;
-
-    % Flip the derivative so the peaks point upwards
-    convoluted2nd = -convoluted2nd;
-
-    % Identify regions where the signal appears
-    peakzones = logical(convoluted2nd);
-
-    % Remove partial peaks at the start of the spectrum
-    finished = false;
-    idx = 1;
-    while ~finished
-        if (idx > length(peakzones))
-            finished = true;
-        else
-            if peakzones(idx)
-                % on a peak, so set this channel to zero (false)
-                peakzones(idx) = false;
-            else
-                % not on a peak, so we can stop now
-                finished = true;
-            end
-            idx = idx + 1;
-        end
-    end
-
-    % Remove partial peaks at the end of the spectrum
-    peakzones = fliplr(peakzones);
-    finished = false;
-    idx = 1;
-    while ~finished
-        if (idx > length(peakzones))
-            finished = true;
-        else
-            if peakzones(idx)
-                % on a peak, so set this channel to zero (false)
-                peakzones(idx) = false;
-            else
-                % not on a peak, so we can stop now
-                finished = true;
-            end
-            idx = idx + 1;
-        end
-    end
-    peakzones = fliplr(peakzones);
-
-    % Now we must have an even number of peak 'starts' and peak 'stops'
-
-    % Identify the peak limits
-    transitions = diff(peakzones);
-    peakstart = find(transitions == 1) + 1;
-    peakstop  = find(transitions == -1);
-    numpeaks = length(peakstart);
-        
-    % Create a location to put the results. Values are index coordinates
-    peaktable = zeros(numpeaks,5);
+    % peak table columns
     begincol = 1;
     endcol = 2;
     midpointcol = 3;
     centroidcol = 4;
     areacol = 5;
 
-    % Populate the peak table
-    peaktable(:,begincol) = peakstart';
-    peaktable(:,endcol) = peakstop';
-
-    % With these peak limits, identify the centres, centroids and areas
-    peaktable(:,midpointcol) = round((peaktable(:,endcol) - peaktable(:,begincol)) / 2) + peaktable(:,begincol);
-
-    for i = 1:numpeaks
-        peaktable(i,centroidcol) = utilities.centroid(spectrum,peaktable(i,begincol),peaktable(i,endcol));
-        peaktable(i,areacol) = sum(spectrum(peaktable(i,begincol):peaktable(i,endcol)),2);
+    %% Identify some data to peak detect
+    if isa(this, 'ChiSpectrum')
+        spectrumtoworkon = this.data;
+        original = this.clone();
+    else
+        spectrumtoworkon = mean(this.data);
+        original = this.clone();
+        original.mean;
     end
 
-    % Select the top N peaks
-    if (numberlimit < numpeaks)
-        peaktable = sortrows(peaktable,areacol,'descend');
-        peaktable = peaktable(1:numberlimit,:);
-        peaktable = sortrows(peaktable,begincol,'ascend');
-        numpeaks = numberlimit;
-    end
+    %% Apply peaklist if present
+    if ~isempty(peaktable)
+        numpeaks = size(peaktable,1);
+    else
+        %% Detect the peaks
+        % Generate a Gaussian smoothing kernel
+        % https://stackoverflow.com/questions/6992213/gaussian-filter-on-a-vector-in-matlab
+        x = linspace(-gwindow/2, gwindow/2, gwindow);
+        gaussFilter = exp(-x.^2 / (2 * gsigma^2));
+        gaussFilter = gaussFilter / sum(gaussFilter);   % normalize
 
-    %% Plot the results for the total spectrum if requested
-    if vis
-        
-        figure('Name','Peak detection results','NumberTitle','off');
-        
-%         s1 = subplot(2,2,1);    
-%         plot(this.mass, utilities.sumnorm(spectrum));
-%         hold on; 
-%         plot(this.mass,utilities.sumnorm(convoluted), 'r');
-%         hold off;
-%         title('Raw data vs. smoothed');
-%         legend({'raw','smoothed'});
-%         xlabel('m/z');
-%         utilities.tightxaxis;
-% 
-%         s2 = subplot(2,2,2);
-%         plot(this.mass, utilities.vectornorm(spectrum));
-%         hold on; 
-%         plot(this.mass,utilities.vectornorm(convoluted2nd), 'r');
-%         hold off;
-%         title('Raw data vs. 2nd deriv');
-%         legend({'raw','2nd deriv'});
-%         xlabel('m/z');
-%         utilities.tightxaxis;
-% 
-%         s3 = subplot(2,2,3);
-%         bar(this.mass(peaktable(:,centroidcol)), utilities.sumnorm(peaktable(:,areacol)'), 'r','EdgeColor', 'r');
-%         hold on; 
-%         plot(this.mass,plotscaler * utilities.sumnorm(spectrum));
-%         hold off;
-%         title('Detected peaks in red');
-%         xlabel('m/z');
-%         ylabel('normalised raw data')
-%         utilities.tightxaxis;
-% 
-%         s4 = subplot(2,2,4);
-        colours = get(gca,'colororder');
+        % Smooth the spectrum using convolution
+        convoluted = conv(spectrumtoworkon,gaussFilter,'same');
 
-        hold on; 
-        bar(this.mass(peaktable(:,centroidcol)), utilities.sumnorm((peaktable(:,areacol))'), 'FaceColor',colours(2,:), 'EdgeColor', colours(2,:));
-        plot(this.mass,plotscaler * utilities.sumnorm(spectrum), 'Color',colours(1,:));
-        hold off;
-        title(['Top ', num2str(numberlimit), ' detected peaks']);
-        xlabel('m/z (amu)');
-        ylabel('normalised raw data')
-        legend({'Detected peaks','Data'}, 'Location','best');
-        utilities.tightxaxis;
+        % Take second derivative of the smoothed spectrum
+        convoluted2nd = utilities.secondderiv(this.mass,convoluted,d2window);
 
-%         linkaxes([s1,s2,s3,s4],'x');
+        % Remove any positive going features
+        convoluted2nd(convoluted2nd > 0) = 0;
 
-        % Manage data cursor information
-        figurehandle = gcf;
-        cursor = datacursormode(figurehandle);
-        set(cursor,'UpdateFcn',{@utilities.datacursor_6sf});
+        % Flip the derivative so the peaks point upwards
+        convoluted2nd = -convoluted2nd;
+
+        % Identify regions where the signal appears
+        peakzones = logical(convoluted2nd);
+
+        % Remove partial peaks at the start of the spectrum
+        finished = false;
+        idx = 1;
+        while ~finished
+            if (idx > length(peakzones))
+                finished = true;
+            else
+                if peakzones(idx)
+                    % on a peak, so set this channel to zero (false)
+                    peakzones(idx) = false;
+                else
+                    % not on a peak, so we can stop now
+                    finished = true;
+                end
+                idx = idx + 1;
+            end
+        end
+
+        % Remove partial peaks at the end of the spectrum
+        peakzones = fliplr(peakzones);
+        finished = false;
+        idx = 1;
+        while ~finished
+            if (idx > length(peakzones))
+                finished = true;
+            else
+                if peakzones(idx)
+                    % on a peak, so set this channel to zero (false)
+                    peakzones(idx) = false;
+                else
+                    % not on a peak, so we can stop now
+                    finished = true;
+                end
+                idx = idx + 1;
+            end
+        end
+        peakzones = fliplr(peakzones);
+
+        % Now we must have an even number of peak 'starts' and peak 'stops'
+
+        % Identify the peak limits
+        transitions = diff(peakzones);
+        peakstart = find(transitions == 1) + 1;
+        peakstop  = find(transitions == -1);
+        numpeaks = length(peakstart);
+
+        %% Create a location to put the results. Values are index coordinates
+        peaktable = zeros(numpeaks,5);
+
+        %% Populate the peak table
+        peaktable(:,begincol) = peakstart';
+        peaktable(:,endcol) = peakstop';
+
+        % With these peak limits, identify the centres, centroids and areas
+        peaktable(:,midpointcol) = round((peaktable(:,endcol) - peaktable(:,begincol)) / 2) + peaktable(:,begincol);
+
+        for i = 1:numpeaks
+            peaktable(i,centroidcol) = utilities.centroid(spectrumtoworkon,peaktable(i,begincol),peaktable(i,endcol));
+            peaktable(i,areacol) = sum(spectrumtoworkon(peaktable(i,begincol):peaktable(i,endcol)),2);
+        end
+
+        %% Select the top N peaks
+        if (numberlimit < numpeaks)
+            peaktable = sortrows(peaktable,areacol,'descend');
+            peaktable = peaktable(1:numberlimit,:);
+            peaktable = sortrows(peaktable,begincol,'ascend');
+            numpeaks = numberlimit;
+        end
 
     end
     
@@ -307,8 +266,27 @@ else
     this.data = datapeaks;
     this.xvals = this.xvals(peaktable(:,centroidcol));
     this.iscentroided = true;
+    this.peaktable = peaktable;    
     
-end
+    %% Plot the results for the total spectrum if requested
+    if vis
+        original.featurenorm.plot;
+        hold on
+        this.featurenorm.plot('nofig','axes',gca);
+        hold off
 
-end
+        title(['Top ', num2str(numpeaks), ' detected peaks']);
+        xlabel('m/z (amu)');
+        ylabel('normalised data')
+        legend({'Data','Detected peaks'}, 'Location','best');
+        utilities.tightxaxis;
 
+        % Manage data cursor information
+        figurehandle = gcf;
+        cursor = datacursormode(figurehandle);
+        set(cursor,'UpdateFcn',{@utilities.datacursor_6sf});
+    end
+
+end % new object required?
+
+end % function

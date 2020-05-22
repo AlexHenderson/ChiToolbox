4959932eda7ab822e1051e55d9d690219de8a962
AlexHenderson <alex.henderson@manchester.ac.uk>
2020-05-22 16:10:40 +0100

    Improved help and comments. Added filename as an output.

--- ChiToolbox/formats/oceanoptics/oceanopticstxtfile.m
+++ ChiToolbox/formats/oceanoptics/oceanopticstxtfile.m
@@ -7,7 +7,7 @@ function [xvals,yvals,info] = oceanopticstxtfile(filename)
 %   [xvals, yvals, info] = oceanopticstxtfile(filename);
 %
 % Purpose:
-%   Extracts a spectrum from an Ocean Optics (now Ocean Insight) text file.
+%   Extracts a spectrum from an Ocean Optics text file.
 %
 %  input:
 %   'filename' string containing the full path to a .txt file (optional)
@@ -19,10 +19,12 @@ function [xvals,yvals,info] = oceanopticstxtfile(filename)
 %   contained in the file
 %
 %                     *******Caution******* 
-%   This code is a hack of the Ocean Optics (now Ocean Insight) format and
-%   so there may be files that cannot be opened using this function.
-%   Always check the output to make sure it is sensible. If you have a file
-%   that doesn't work, please contact Alex.
+%   This code only opens a single spectrum. 
+% 
+%   This code is a hack of the Ocean Optics format and so there may be
+%   files that cannot be opened using this function. Always check the
+%   output to make sure it is sensible. If you have a file that doesn't
+%   work, please contact Alex.
 %
 %   Copyright (c) 2020, Alex Henderson 
 %   Contact email: alex.henderson@manchester.ac.uk
@@ -40,63 +42,90 @@ function [xvals,yvals,info] = oceanopticstxtfile(filename)
 % Requires MATLAB R2016b or greater (split command)
 
 
+% Get the filename if not supplied
+if ~exist('filename', 'var')
+    filename = getfilename('*.txt', 'Ocean Optics Files (*.txt)');
+    if (isfloat(filename) && (filename == 0))
+        return;
+    end
+    filename = filename{1};
+end
+
+% Ensure we only have a single filename and it is a string
 if iscell(filename)
     filename = filename{1};
 end
 
-info = cell(1,2);
-
-info{1,1} = 'Description';
-info{1,2} = '';
-info{2,1} = 'Filename';
-info{2,2} = filename;
-
-xvals = [];
-yvals = [];
-
-try
-fid = fopen(filename, 'rt', 'ieee-le');
-
-finishedheader = false;
-firstpass = true;
-while ~finishedheader
-    line = fgetl(fid);
-    if (line == -1)
-        throw 'end of file?'
-    end
-        
-    if strcmpi(line, '>>>>>Begin Spectral Data<<<<<')
-        finishedheader = true;
-    else
-        if firstpass
-            info{1,2} = line;
-            firstpass = false;
-        else
-            % Parse header lines
-            splitline = split(line,': ');
-            if strcmpi(splitline{1}, 'ate')
-                splitline{1} = 'Date';
-            end
-            info = vertcat(info,{splitline{1},splitline{2}}); %#ok<AGROW>
-        end
-    end
+% Quit now, if we can't get any further
+if (exist(filename, 'file') ~= 2)
+    err = MException(['CHI:',mfilename,':IOError'], ...
+        'File does not exist');
+    throw(err);
 end
 
-% Read the data
-data = textscan(fid, '%f\t%f');
-data = cell2mat(data);
+% Make space for the info
+info = cell(1,2);
 
-xvals = data(:,1)';
-yvals = data(:,2)';
+% Add some info that is either not in the file (like the filename) or
+% cannot be easiily extrracted (because the file format is a little broken)
+info{1,1} = 'Description';
+info{1,2} = '';
 
-fclose(fid);
+info{2,1} = 'FullFilename';
+info{2,2} = filename;
 
-catch ex
-    
+info{3,1} = 'Filename';
+[filepath,name] = fileparts(filename); %#ok<ASGLU>
+info{3,2} = name;
+
+% Open the file and grab the contents
+try
+    fid = fopen(filename, 'rt', 'ieee-le');
+
+    % Read the header
+    finishedheader = false;
+    firstpass = true;
+    while ~finishedheader
+        line = fgetl(fid);
+        if (line == -1)
+            throw 'end of file?'
+        end
+
+        if strcmpi(line, '>>>>>Begin Spectral Data<<<<<')
+            finishedheader = true;
+        else
+            if firstpass
+                info{1,2} = line;
+                firstpass = false;
+            else
+                % Parse header lines
+                splitline = split(line,': ');
+                % Workaround for broken line endings
+                if strcmpi(splitline{1}, 'ate')
+                    splitline{1} = 'Date';
+                end
+                info = vertcat(info,{splitline{1},splitline{2}}); %#ok<AGROW>
+            end
+        end
+    end
+
+    % Read the data
+    data = textscan(fid, '%f\t%f');
+    data = cell2mat(data);
+
+    % Transpose to rows and separate the x and y values
+    xvals = data(:,1)';
+    yvals = data(:,2)';
+
+    fclose(fid);
+
+catch ex    
+    % Close the file if it was ever opened
     if exist('fid','var')
         fclose(fid);
     end    
-    
+    % Pass the error handling up the chain
+    rethrow(ex);
 end
 
-end
+end     % end function

d1b5224a45ec243c83e4ec3938da6e4b762916ef
AlexHenderson <alex.henderson@manchester.ac.uk>
2020-07-11 11:26:47 +0100

    Added ability to predict the class of previously unseen data.

---
+++ ChiToolbox/@ChiCVAPrediction/ChiCVAPrediction.m
@@ -0,0 +1,138 @@
+classdef ChiCVAPrediction < ChiBase
+
+% ChiCVAPrediction  Class to manage the outcome of CVA prediction.
+%
+% Syntax
+%   outcome =
+%   ChiCVAPrediction(model,projectedscores,pcs,elapsed,distances, ...
+%                        predictedclass,trueclass,correctlyclassified)
+%   outcome = ChiCVAPrediction(____,history)
+% 
+% Description
+%   outcome =
+%   ChiCVAPrediction(model,projectedscores,pcs,elapsed,distances,
+%   predictedclass,trueclass,correctlyclassified) creates a wrapper for the
+%   outcome of a prediction by a PCA model on a set of 'previously unseen'
+%   data.
+% 
+%   outcome = ChiCVAPrediction(____,history) includes a ChiLogger history
+%   object recording the data processing history.
+% 
+% Notes
+%   If there are class membership labels available, a better method of
+%   classification is Canonical Variates Analysis which takes the a priori
+%   information into account. See ChiSpectralCVAModel for more information. 
+% 
+% Copyright (c) 2020, Alex Henderson.
+% Licenced under the GNU General Public License (GPL) version 3.
+%
+% See also 
+%   pca ChiSpectralPCAModel cva ChiSpectralCVAModel randomforest adaboost.
+
+% Contact email: alex.henderson@manchester.ac.uk
+% Licenced under the GNU General Public License (GPL) version 3
+% http://www.gnu.org/copyleft/gpl.html
+% Other licensing options are available, please contact Alex for details
+% If you use this file in your work, please acknowledge the author(s) in
+% your publications. 
+
+% The latest version of this file is available on Bitbucket
+% https://bitbucket.org/AlexHenderson/chitoolbox
+
+
+    properties
+        model;      % CVA model from which the prediction was made
+        projectedscores;    % predicted CVA scores of the test set.
+        elapsed;    % time in seconds that the prediction took
+        distances;  % Mahalanobis distance of each test spectrum to each class
+        predictedclass; % a list of label identifiers indicating the outcome of prediction.
+        trueclass; % a list of label identifiers indicating the outcome of prediction.
+        correctlyclassified;    % if the unseen data was labelled, this is a vector indicating whether it was correctly classified. Otherwise it is an empty variable.
+        history = ChiLogger();  % log of data processing steps
+    end
+    
+    properties (Dependent = true)
+        cc; % if the unseen data was labelled, this is a vector indicating whether it was correctly classified. Otherwise it is an empty variable.
+        percentcorrectlyclassified; % if the unseen data was labelled, this is the percentage of predicted data correctly classified. 
+        percentcc; % if the unseen data was labelled, this is the percentage of predicted data correctly classified. 
+        pcc; % if the unseen data was labelled, this is the percentage of predicted data correctly classified. 
+        predictedlabel; % predicted class label
+        truelabel;      % true class label
+        elapsedstr;     % time that the prediction took as a string
+    end
+    
+    methods
+        %% Constructor
+        function this = ChiCVAPrediction(...
+                            model, ...
+                            projectedscores, ...
+                            elapsed, ...
+                            distances, ...
+                            predictedclass, ...
+                            trueclass, ...
+                            correctlyclassified, ...
+                            varargin ...
+                        )
+                    
+            argposition = find(cellfun(@(x) isa(x,'ChiLogger') , varargin));
+            if argposition
+                this.history = varargin{argposition}.clone;
+                varargin(argposition) = [];  %#ok<NASGU>
+            else
+                this.history = ChiLogger();
+            end
+            
+            if nargin % Support calling with 0 arguments
+                this.model = model.clone();
+                this.projectedscores = projectedscores;
+                this.elapsed = elapsed;
+                this.distances = distances;
+                this.predictedclass = predictedclass;
+                this.trueclass = trueclass;
+                this.correctlyclassified = correctlyclassified;
+            end 
+
+        end
+        
+        %% percentcc
+        function percentcorrectlyclassified = get.percentcorrectlyclassified(this)
+            if ~isempty(this.correctlyclassified)
+                percentcorrectlyclassified = 100 * sum(this.correctlyclassified) ./ length(this.correctlyclassified);
+            else
+                percentcorrectlyclassified = [];
+            end
+        end
+        
+        %% percentcc
+        function percentcc = get.percentcc(this)
+            percentcc = this.percentcorrectlyclassified;
+        end
+        
+        %% cc
+        function cc = get.cc(this)
+            cc = this.correctlyclassified;
+        end
+       
+        %% pcc
+        function pcc = get.pcc(this)
+            pcc = this.percentcc;
+        end
+       
+        %% truelabel
+        function truelabel = get.truelabel(this)
+            truelabel = this.model.pca.classmembership.uniquelabels(this.trueclass);
+        end
+       
+        %% predictedlabel
+        function predictedlabel = get.predictedlabel(this)
+            predictedlabel = this.model.pca.classmembership.uniquelabels(this.predictedclass);
+        end
+       
+        %% elapsedstr
+        function elapsedstr = get.elapsedstr(this)
+            elapsedstr = durationString(this.elapsed);
+        end
+       
+    end
+    
+end
---
+++ ChiToolbox/@ChiCVAPrediction/plotscores.m
@@ -0,0 +1,177 @@
+function plotscores(this,cvx,cvy,varargin)
+
+% plotscores  Plots canonical variate scores overlaid with projected data
+%
+% Syntax
+%   plotscores(cvx,cvy);
+%   plotscores(cvx,cvy,'nofig');
+%
+% Description
+%   plotscores(cvx,cvy) creates a 2-D scatter plot of canonical variate
+%   scores. Predicted scores from the previously unseen data are overlaid
+%   using a different marker (default = '*'). 
+%   If the predicted data had a classmembership, the predicted markers are
+%   shown in their 'true' colour. If the predicted data had no
+%   classmembership the markers are classified as 'undefined label' and
+%   shown in black.
+%   cvx is the canonical variate number to plot on the x-axis, while cvy is
+%   the canonical variate number to plot on the y-axis. A new figure window
+%   is created.
+%
+%   plotscores(cvx,cvy,'nofig') plots the scores in the currently active
+%   figure window, or creates a new figure if none is available.
+%
+%   Other parameters can be applied to customise the plot. See the MATLAB
+%   scatter, or utilities.gscatter, functions for more details. 
+%
+% Notes
+%   If the unseen data has no class variable, the projection will be
+%   plotted using 'undefined label' and plotted in black. 
+%
+% Copyright (c) 2020, Alex Henderson.
+% Licenced under the GNU General Public License (GPL) version 3.
+%
+% See also 
+%   scatter utilities.gscatter ChiSpectralCVAModel ChiSpectralCollection.
+
+% Contact email: alex.henderson@manchester.ac.uk
+% Licenced under the GNU General Public License (GPL) version 3
+% http://www.gnu.org/copyleft/gpl.html
+% Other licensing options are available, please contact Alex for details
+% If you use this file in your work, please acknowledge the author(s) in
+% your publications. 
+
+% The latest version of this file is available on Bitbucket
+% https://bitbucket.org/AlexHenderson/Chitoolbox
+
+titlestub = 'Scores on canonical variates ';
+windowtitlestub = titlestub;
+axislabelstub = 'score on CV ';
+
+% Some defaults
+marker = '.';
+pmarker = '*';
+sizedata = 6;
+sizedatadefined = false;
+
+if ((cvx > this.model.numcvs) || (cvx < 1))
+    err = MException(['CHI:',mfilename,':OutOfRange'], ...
+        ['Requested canonical variate is out of range. Max CVs = ', num2str(this.model.numcvs), '.']);
+    throw(err);
+end
+if ((cvy > this.model.numcvs) || (cvy < 1))
+    err = MException(['CHI:',mfilename,':OutOfRange'], ...
+        ['Requested canonical variate is out of range. Max CVs = ', num2str(this.model.numcvs), '.']);
+    throw(err);
+end
+
+argposition = find(cellfun(@(x) strcmpi(x, 'nofig') , varargin));
+if argposition
+    % Remove the parameter from the argument list
+    varargin(argposition) = [];
+else
+    % No 'nofig' found so create a new figure
+    windowtitle = [windowtitlestub, num2str(cvx), ' and ' num2str(cvy)];
+    figure('Name',windowtitle,'NumberTitle','off');
+end    
+
+% sizedata
+argposition = find(cellfun(@(x) strcmpi(x, 'sizedata'), varargin));
+if argposition
+    sizedata = varargin{argposition+1};
+    sizedatadefined = true;
+    varargin(argposition+1) = [];
+    varargin(argposition) = [];
+end
+
+% projected data marker
+% Do this before looking for the marker since we can't search for two
+% different string
+argposition = find(cellfun(@(x) strcmpi(x, 'pmarker'), varargin));
+if argposition
+    pmarker = varargin{argposition +1};
+    varargin(argposition+1) = [];
+    varargin(argposition) = [];
+end
+
+% marker
+argposition = find(cellfun(@(x) ischar(x) , varargin));
+if argposition
+    marker = varargin{argposition};
+    varargin(argposition) = [];
+end
+
+% If the chosen marker is a dot and the size has not been defined, make the
+% dot a bit bigger. 
+if (strcmp(marker, '.') && ~sizedatadefined)
+    sizedata = 15;
+end
+
+colours = get(gca,'colororder');
+axiscolour = 'k';
+decplaces = 3;
+
+projectedcolours = colours;
+projectedlabels = this.truelabel;
+
+if isempty(this.truelabel)
+    projectedcolours = 'k';
+    projectedlabels = cell(size(this.projectedscores,1),1);
+    projectedlabels(:) = {'undefined label'};    
+end
+
+if ~isempty(this.model.pca.classmembership)
+    utilities.gscatter(this.model.scores(:,cvx), this.model.scores(:,cvy), this.model.pca.classmembership.labels, 'colours', colours, 'sizedata', sizedata, marker, 'nofig', varargin{:});
+    hold on;
+    utilities.gscatter(this.projectedscores(:,cvx), this.projectedscores(:,cvy), projectedlabels, 'colours', projectedcolours, 'sizedata', sizedata, pmarker, 'nofig', varargin{:});
+    hold off;
+else
+    scatter(this.model.scores(:,cvx), this.model.scores(:,cvy), sizedata .* sizedata, marker, varargin{:});
+    hold on;
+    scatter(this.projectedscores(:,cvx), this.projectedscores(:,cvy), sizedata .* sizedata, pmarker, varargin{:});
+    hold off;
+end    
+
+xlabel([axislabelstub, num2str(cvx), ' (', num2str(this.model.explained(cvx),decplaces), '%)']);
+ylabel([axislabelstub, num2str(cvy), ' (', num2str(this.model.explained(cvy),decplaces), '%)']);
+title([titlestub, num2str(cvx), ' and ', num2str(cvy)]);
+
+% Draw lines indicating zero x and y
+hold on;
+limits = axis;
+xmin = limits(1,1);
+xmax = limits(1,2);
+ymin = limits(1,3);
+ymax = limits(1,4);
+
+h = plot([0,0], [0,ymax], axiscolour);
+set(get(get(h,'Annotation'),'LegendInformation'),'IconDisplayStyle','off');
+set(h,'HitTest','off'); % Prevent datatips on this line
+h = plot([0,0], [0,ymin], axiscolour);
+set(get(get(h,'Annotation'),'LegendInformation'),'IconDisplayStyle','off');
+set(h,'HitTest','off'); % Prevent datatips on this line
+h = plot([0,xmax], [0,0], axiscolour);
+set(get(get(h,'Annotation'),'LegendInformation'),'IconDisplayStyle','off');
+set(h,'HitTest','off'); % Prevent datatips on this line
+h = plot([0,xmin], [0,0], axiscolour);
+set(get(get(h,'Annotation'),'LegendInformation'),'IconDisplayStyle','off');
+set(h,'HitTest','off'); % Prevent datatips on this line
+axis tight
+hold off;
+
+%% Manage data cursor information
+plotinfo = struct;
+plotinfo.xpointlabel = ['CV ', num2str(cvx)];
+plotinfo.ypointlabel = ['CV ', num2str(cvy)];
+plotinfo.xdata = this.model.scores(:,cvx);
+plotinfo.ydata = this.model.scores(:,cvy);
+
+if ~isempty(this.model.pca.classmembership)
+    plotinfo.pointmembershiplabels = this.model.pca.classmembership.labels;
+end
+
+figurehandle = gcf;
+cursor = datacursormode(figurehandle);
+set(cursor,'UpdateFcn',{@utilities.datacursor_scores_6sf,this,plotinfo});
+    
+end

acadf24c70a5a8e57bc6d86685845324e89d1346
AlexHenderson <alex.henderson@manchester.ac.uk>
2020-07-21 16:37:01 +0100

    Added a property that calculates the sum of all spectra.

---
+++ ChiToolbox/@ChiSpectralCollection/totalspectrum.m
@@ -0,0 +1,10 @@
+function spectrum = totalspectrum(this)
+% Sum of spectra
+% Copyright (c) 2019 Alex Henderson (alex.henderson@manchester.ac.uk)
+
+    spectrumclass = str2func(this.spectrumclassname);
+    spectrum = spectrumclass(this.xvals,sum(this.data),this.reversex,this.xlabelname,this.xlabelunit,this.ylabelname,this.ylabelunit);
+    spectrum.history.add('Sum of spectra');
+    this.history.add('Sum of spectra');
+
+end
